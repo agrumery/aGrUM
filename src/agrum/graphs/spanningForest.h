016037b5580a00ed5d5b6507c0a96b74a3dcd650
speedy <speedy@fa2884e2-1fde-468d-ae8a-4cc2b5279982>
2009-11-01 10:56:36 +0000

    adding a min cost spanning tree/forest algorithm
    git-svn-id: https://alm.lip6.fr/svn/agrum/trunk@293 fa2884e2-1fde-468d-ae8a-4cc2b5279982

---
+++ src/agrum/graphs/spanningForest.h
@@ -0,0 +1,100 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Christophe GONZALES and Pierre-Henri WUILLEMIN  *
+ *   {prenom.nom}_at_lip6.fr                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/** @file
+ * @brief Interface for computing min cost spanning trees or forests
+ *
+ * @author Jean-Philippe DUBUS and Christophe GONZALES
+ */
+#ifndef GUM_SPANNING_FOREST_H
+#define GUM_SPANNING_FOREST_H
+
+#include <agrum/graphs/graphElements.h>
+#include <agrum/graphs/undiGraph.h>
+
+
+namespace gum {
+  
+
+  /* =========================================================================== */
+  /** @class SpanningForest
+   * @brief Base class for computing min cost spanning trees or forests */
+  /* =========================================================================== */
+  class SpanningForest {
+  public:
+    // ############################################################################
+    /// @name Constructors / Destructors
+    // ############################################################################
+    /// @{
+
+    /// Destructor
+    virtual ~SpanningForest();
+
+    /// @}
+
+    
+    // ############################################################################
+    /// @name Accessors / Modifiers
+    // ############################################################################
+    /// @{
+
+    /// Returns the edges in a min cost spanning forest
+    /** @returns edges in the spanning forest */
+    virtual const EdgeSet& edgesInSpanningForest() = 0;
+
+    /// Construct the spanning forest
+    /** @return the spanning forest */
+    virtual const UndiGraph& spanningForest() = 0;
+
+    /// Returns the cost of the spanning forest
+    /** @return cost of the spanning forest */
+    virtual float costOfSpanningForest() = 0;
+
+    /// @}
+    
+
+  protected:
+    // ############################################################################
+    /// @name Constructors / Destructors
+    // ############################################################################
+    /// @{
+
+    /// default constructor
+    /** You should use SpanningForestPrim or SpanningForestKuskal to create
+     * appropriate spanning trees algorithms. */
+    SpanningForest();
+
+    /// Copy constructor
+    SpanningForest(const SpanningForest& toCopy);
+
+    /// Copy operator
+    /** avoid copying the interface from scratch: this would prevent, in
+     * particular, that a Prim algorithm be initialized by a Kruskal algo. */
+    SpanningForest& operator=(const SpanningForest& toCopy);
+
+    /// @}
+
+  };
+
+  
+} /* namespace gum */
+
+
+#endif /* GUM_SPANNING_FOREST_H */
+
---
+++ src/agrum/graphs/spanningForestPrim.cpp
@@ -0,0 +1,163 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Christophe GONZALES and Pierre-Henri WUILLEMIN  *
+ *   {prenom.nom}_at_lip6.fr                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/** @file
+ * @brief The Prim algorithm for computing min cost spanning trees or forests
+ *
+ * @author Jean-Philippe DUBUS and Christophe GONZALES
+ */
+
+
+#include <agrum/core/debug.h>
+#include <agrum/core/exceptions.h>
+#include <agrum/graphs/spanningForestPrim.h>
+
+
+namespace gum {
+  
+
+  /// Default constructor
+  SpanningForestPrim::SpanningForestPrim ( const UndiGraph* graph,
+                                           const Property<float>::onEdges* cost ) :
+    SpanningForest (),
+    __graph ( *graph ),
+    __costTable ( *cost ),
+    __spanning_tree_cost ( 0 ),
+    __require_computation ( true ) {
+    if ( ! graph || ! cost ) {
+      GUM_ERROR( GraphError, "invalid null graph or edge cost pointer" );
+    }
+    
+    // for debugging purposes
+    GUM_CONSTRUCTOR(SpanningForestPrim);
+  }
+
+
+  // copy constructor
+  SpanningForestPrim::SpanningForestPrim (const SpanningForestPrim& from ) :
+    SpanningForest (),
+    __graph ( from.__graph ),
+    __costTable ( from.__costTable ),
+    __edgesToExplore ( from.__edgesToExplore ),
+    __spanning_tree ( from.__spanning_tree ),
+    __spanning_tree_cost ( from.__spanning_tree_cost ),
+    __require_computation ( from.__require_computation ) {
+    // for debugging purposes
+    GUM_CONS_CPY(SpanningForestPrim);
+  }
+
+  
+  // destructor
+  SpanningForestPrim::~SpanningForestPrim() {
+    // for debugging purposes
+    GUM_DESTRUCTOR(SpanningForestPrim);
+  }
+
+
+  /// Returns the cost of the spanning forest
+  float SpanningForestPrim::costOfSpanningForest() {
+    if ( __require_computation ) __compute ();
+    return __spanning_tree_cost;
+  }
+
+
+  /// Returns the edges in a min cost spanning forest
+  const EdgeSet& SpanningForestPrim::edgesInSpanningForest () {
+    if ( __require_computation ) __compute ();
+    return __spanning_tree.edges();
+  }
+
+
+  /// Construct the spanning forest
+  const UndiGraph& SpanningForestPrim::spanningForest() {
+    if ( __require_computation ) __compute ();
+    return __spanning_tree;
+  }
+  
+
+  /// compute the spanning forest
+  void SpanningForestPrim::__compute() {
+    // compute a spanning tree in every connected component
+    for ( UndiGraph::NodeIterator iter = __graph.beginNodes();
+          iter != __graph.endNodes(); ++iter) {
+      if ( ! __spanning_tree.existsNode ( *iter ) ) {
+        __computeInAComponent ( *iter );
+      }
+    }
+
+    // indicate that everything was computed
+    __require_computation = false;
+  }
+
+
+  /// compute a spanning tree
+  void SpanningForestPrim::__computeInAComponent ( const NodeId id ) {
+    // add the node to the spanning tree
+    __spanning_tree.insertNode ( id );
+
+    // explore its neighborhood
+    __exploreNode( id );
+
+    // get the next nodes to link to the current spanning tree nodes
+    while( ! __edgesToExplore.empty() ) {
+      const Edge edge = __edgesToExplore.pop();
+      const NodeId first = edge.first();
+      const NodeId second = edge.second();
+
+      // consider only the edges that have one extremal node not in the spanning
+      // tree as those that can be added to the tree
+      if ( ! __spanning_tree.existsNode ( first ) ) {
+        // add the edge to the spanning tree
+        __spanning_tree.insertNode ( first );
+        __spanning_tree.insertEdge ( first, second );
+        __spanning_tree_cost += __costTable[edge];
+ 
+       // We must explore the first node's neighborhood
+        __exploreNode ( first );
+      }
+      else if ( ! __spanning_tree.existsNode ( second ) ) {
+        // add the edge to the spanning tree
+        __spanning_tree.insertNode ( second );
+        __spanning_tree.insertEdge ( first, second );
+        __spanning_tree_cost += __costTable[edge];
+      
+        // We must explore the second node
+        __exploreNode ( second );
+      }
+    }
+  }
+
+
+  /// explore the neighborhood of a node belonging to the spanning tree
+  void SpanningForestPrim::__exploreNode ( const NodeId id ) {
+    // add its neighbors __edgesToExplore to indicate that they are
+    // potential next nodes to explore
+    const Set<Edge>& neighbours = __graph.neighbours ( id );
+    for ( Set<Edge>::const_iterator iter = neighbours.begin();
+          iter != neighbours.end(); ++iter ) {
+      const Edge& edge = *iter;
+      if ( ! __spanning_tree.existsNode ( edge.other( id ) ) ) {
+        __edgesToExplore.insert ( __costTable[edge], edge );
+      }
+    }
+  }
+
+
+} /* namespace gum */
+
