5b4ded0c4e3c5253eae29942a122878a8b4dd861
vincent <vincent@fa2884e2-1fde-468d-ae8a-4cc2b5279982>
2011-01-24 13:21:38 +0000

    Update SkoorTestSuite.
    git-svn-id: https://alm.lip6.fr/svn/agrum/trunk@569 fa2884e2-1fde-468d-ae8a-4cc2b5279982

--- src/testunits/SkoorTestSuite.h
+++ src/testunits/SkoorTestSuite.h
@@ -57,122 +57,121 @@ class SkoorTestSuite: public CxxTest::TestSuite {
     
     
     void testParseFile2() {
-      //SkoorInterpreter * si = 0;
+      SkoorInterpreter * si = 0;
       
-      //TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/query2.skoor") );
+      TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/query2.skoor") );
       
-      //si->setSyntaxMode(true);
-      //TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
+      si->setSyntaxMode(true);
+      TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
       
-      //TS_ASSERT_EQUALS( si->errors(), 3 );
-      //TS_ASSERT_EQUALS( si->warnings(), 0 );  
+      TS_ASSERT_EQUALS( si->errors(), 3 );
+      TS_ASSERT_EQUALS( si->warnings(), 0 );  
       
-      //delete si;
+      delete si;
     }
     
 
     void testInference() {
-      //SkoorInterpreter * si = 0;
+      SkoorInterpreter * si = 0;
       
-      //TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/query1.skoor") );
-      //TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
+      TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/query1.skoor") );
+      TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
       
-      //TS_ASSERT_EQUALS( si->errors(), 0 );
-      //TS_ASSERT_EQUALS( si->warnings(), 0 );
+      TS_ASSERT_EQUALS( si->errors(), 0 );
+      TS_ASSERT_EQUALS( si->warnings(), 0 );
       
-      //delete si;
+      delete si;
     }
     
     
     void testObserve() {
-      //SkoorInterpreter * si = 0;
+      SkoorInterpreter * si = 0;
       
-      //TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/queryObserveTest.skoor") );
-      //TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
+      TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/queryObserveTest.skoor") );
+      TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
 
-      //TS_ASSERT_EQUALS( si->errors(), 0 );
-      //TS_ASSERT_EQUALS( si->warnings(), 0 );
+      TS_ASSERT_EQUALS( si->errors(), 0 );
+      TS_ASSERT_EQUALS( si->warnings(), 0 );
       
-      //// Observe correctly
-      //const Instance& c1 = si->prm()->getSystem("systems.MyKickAssSystem").get("c1");
-      //const Instance& c2 = si->prm()->getSystem("systems.MyKickAssSystem").get("c2");
+      // Observe correctly
+      const Instance& c1 = si->prm()->getSystem("systems.MyKickAssSystem").get("c1");
+      const Instance& c2 = si->prm()->getSystem("systems.MyKickAssSystem").get("c2");
       
-      //TS_ASSERT(   si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("can_print"))) );
-      //TS_ASSERT(   si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("equipState"))) );
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("equipState"))) );
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("can_print"))) ); 
+      TS_ASSERT(   si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("can_print"))) );
+      TS_ASSERT(   si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("equipState"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("equipState"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("can_print"))) ); 
       
-      //const PRMInference::EMap & e = si->inference()->evidence( c2 );
-      //const Potential<prm_float> * p = e[c1.get("equipState").id()];
+      const PRMInference::EMap & e = si->inference()->evidence( c2 );
+      const Potential<prm_float> * p = e[c1.get("equipState").id()];
       
-      //gum::Instantiation j( *p );
-      //const Attribute & c2_equipState = c2.get("equipState");
-      //for ( j.setFirst(); ! j.end(); j.inc() ) {
-        //if ( c2_equipState.type().variable().label( j.val(c2_equipState.type().variable()) ) == "Dysfunctional" ) {
-          //TS_ASSERT_EQUALS( p->get(j), 1.0 );
-        //} else {
-          //TS_ASSERT_EQUALS( p->get(j), 0.0 );
-        //}
-      //}
+      gum::Instantiation j( *p );
+      const Attribute & c2_equipState = c2.get("equipState");
+      for ( j.setFirst(); ! j.end(); j.inc() ) {
+        if ( c2_equipState.type().variable().label( j.val(c2_equipState.type().variable()) ) == "Dysfunctional" ) {
+          TS_ASSERT_EQUALS( p->get(j), 1.0 );
+        } else {
+          TS_ASSERT_EQUALS( p->get(j), 0.0 );
+        }
+      }
       
-      //delete p;
-      //delete si;
+      delete si;
     }
     
     void testUnobserve() {
-      //SkoorInterpreter * si = 0;
+      SkoorInterpreter * si = 0;
       
-      //TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/queryUnobserveTest.skoor") );
-      //TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
+      TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/queryUnobserveTest.skoor") );
+      TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
 
-      //TS_ASSERT_EQUALS( si->errors(), 0 );
-      //TS_ASSERT_EQUALS( si->warnings(), 0 );
+      TS_ASSERT_EQUALS( si->errors(), 0 );
+      TS_ASSERT_EQUALS( si->warnings(), 0 );
       
-      //// Unobserve correctly
-      //const Instance& c1 = si->prm()->getSystem("systems.MyKickAssSystem").get("c1");
-      //const Instance& c2 = si->prm()->getSystem("systems.MyKickAssSystem").get("c2");
+      // Unobserve correctly
+      const Instance& c1 = si->prm()->getSystem("systems.MyKickAssSystem").get("c1");
+      const Instance& c2 = si->prm()->getSystem("systems.MyKickAssSystem").get("c2");
       
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("can_print"))) );
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("equipState"))) );
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("equipState"))) );
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("can_print"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("can_print"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("equipState"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("equipState"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("can_print"))) );
       
-      //delete si;
+      delete si;
     }
     
     void testQuery() {
-      //SkoorInterpreter * si = 0;
+      SkoorInterpreter * si = 0;
       
-      //TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/query1.skoor") );
-      //TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
+      TS_GUM_ASSERT_THROWS_NOTHING( si = new SkoorInterpreter("../../../src/testunits/ressources/skoor/requests/query1.skoor") );
+      TS_GUM_ASSERT_THROWS_NOTHING( si->interpret() );
       
-      //TS_ASSERT_EQUALS( si->errors(), 0 );
-      //TS_ASSERT_EQUALS( si->warnings(), 0 );
+      TS_ASSERT_EQUALS( si->errors(), 0 );
+      TS_ASSERT_EQUALS( si->warnings(), 0 );
       
-      //const Instance& c1 = si->prm()->getSystem("systems.MyKickAssSystem").get("c1");
-      //const Instance& c2 = si->prm()->getSystem("systems.MyKickAssSystem").get("c2");
+      const Instance& c1 = si->prm()->getSystem("systems.MyKickAssSystem").get("c1");
+      const Instance& c2 = si->prm()->getSystem("systems.MyKickAssSystem").get("c2");
       
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("can_print"))) );
-      //TS_ASSERT(   si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("equipState"))) );
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("equipState"))) );
-      //TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("can_print"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("can_print"))) );
+      TS_ASSERT(   si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("equipState"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c1, &c1.get("equipState"))) );
+      TS_ASSERT( ! si->inference()->hasEvidence( PRMInference::Chain(&c2, &c2.get("can_print"))) );
       
-      //// Est-ce que la valeur Dysfonctionnal de l'attribut can_print de l'instance c1 est à 1.0
+      // Est-ce que la valeur Dysfonctionnal de l'attribut can_print de l'instance c1 est à 1.0
       
-      ///*const PRMInference::EMap & e = si->inference()->evidence( c2 );
-      //const Potential<prm_float> * p = e[c1.get("equipState").id()];
+      /*const PRMInference::EMap & e = si->inference()->evidence( c2 );
+      const Potential<prm_float> * p = e[c1.get("equipState").id()];
       
-      //gum::Instantiation j( *p );
-      //const Attribute & c2_equipState = c2.get("equipState");
-      //for ( j.setFirst(); ! j.end(); j.inc() ) {
-        //if ( c2_equipState.type().variable().label( j.val(c2_equipState.type().variable()) ) == "Dysfunctional" ) {
-          //TS_ASSERT_EQUALS( p->get(j), 1.0 );
-        //} else {
-          //TS_ASSERT_EQUALS( p->get(j), 0.0 );
-        //}
-      //}*/  
+      gum::Instantiation j( *p );
+      const Attribute & c2_equipState = c2.get("equipState");
+      for ( j.setFirst(); ! j.end(); j.inc() ) {
+        if ( c2_equipState.type().variable().label( j.val(c2_equipState.type().variable()) ) == "Dysfunctional" ) {
+          TS_ASSERT_EQUALS( p->get(j), 1.0 );
+        } else {
+          TS_ASSERT_EQUALS( p->get(j), 0.0 );
+        }
+      }*/  
       
-      //delete si;
+      delete si;
     }
 };
 
