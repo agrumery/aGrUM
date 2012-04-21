960bf4a4fe6f8be643e984113fe1e091a5154ac7
phw <phw@fa2884e2-1fde-468d-ae8a-4cc2b5279982>
2012-04-21 12:43:14 +0000

    - add experimental sophistications for inline (INLINE / ENFORCED_INLINE / ALWAYS_INLINE / NEVER_INLINE) - add several macros : GUM_MAJOR/MINOR/PATCH_VERSION, GUM_AT_LEAST, GNUC_AT_LEAST, GNUC_AT_MOST
    git-svn-id: https://alm.lip6.fr/svn/agrum/trunk@861 fa2884e2-1fde-468d-ae8a-4cc2b5279982

--- src/agrum/core/inline.h
+++ src/agrum/core/inline.h
@@ -23,6 +23,12 @@
  * aGrUM's INLINE is a substitute to the usual C++ inline. It enables compiling
  * aGrUM with or without inline functions, depending on whether we wish to
  * debug parts of aGrUM.
+ * 
+ * From eigen library, aGrUM proposes now:
+ * INLINE : the compiler has to try to inline
+ * STRONG_INLINE : the compiler has to try harder toinline
+ * ALWAYS_INLINE : the compiler has to inline
+ * NO_INLINE : the compiler should not inline
  *
  * @author Christophe GONZALES and Pierre-Henri WUILLEMIN
  */
@@ -32,9 +38,45 @@
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 
 #ifndef GUM_NO_INLINE
+
 #define INLINE inline
+
+#define ENFORCED_INLINE inline
+// ENFORCED_INLINE is a stronger version of the inline, using __forceinline on MSVC,
+// but it still doesn't use GCC's always_inline. This is useful in (common) situations where MSVC needs forceinline
+// but GCC is still doing fine with just inline.
+// (from eigen library)
+#if (defined _MSC_VER) || (defined __INTEL_COMPILER)
+#define ENFORCED_INLINE __forceinline
+#else
+#define ENFORCED_INLINE inline
+#endif
+
+// ALWAYS_INLINE is the strongest, it has the effect of making the function inline and adding every possible
+// attribute to maximize inlining. This should only be used when really necessary: in particular,
+// it uses __attribute__((always_inline)) on GCC, which most of the time is useless and can severely harm compile times.
+// FIXME with the always_inline attribute,
+// gcc 3.4.x reports the following compilation error:
+//   Eval.h:91: sorry, unimplemented: inlining failed in call to '...' : function body not available
+// (from eigen library)
+#if GNUC_AT_LEAST(4,0)
+#define ALWAYS_INLINE __attribute__((always_inline)) inline
+#else
+#define ALWAYS_INLINE EIGEN_STRONG_INLINE
+#endif
+
+#if (defined __GNUC__)
+#define NO_INLINE __attribute__((noinline))
+#elif (defined _MSC_VER)
+#define NEVER_INLINE __declspec(noinline)
+#else
+#define NEVER_INLINE
+#endif
 #else
 #define INLINE
+#define STRONG_INLINE
+#define ALWAYS_INLINE inline
+#define NEVER_INLINE
 #endif /* GUM_NO_INLINE */
 
 #endif /* DOXYGEN_SHOULD_SKIP_THIS */
