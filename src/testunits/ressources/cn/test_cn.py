aa264a8357b50c73f88bc0e713b3367680558403
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2013-03-11 15:34:07 +0100

    CN in pyAgrum

---
+++ src/testunits/ressources/cn/test_cn.py
@@ -0,0 +1,11 @@
+import pyAgrum as gum
+
+cn=gum.CredalNet("2Umin.bif","2Umax.bif")
+cn.intervalToCredal(0)
+
+ie=gum.MCSampling(cn)
+ie.insertEvidence("L2U.evi")
+
+mcSampling.setRepetitiveInd(False)
+mcSampling.setTimeLimit(1)
+mcSampling.setIterStop(1000)
