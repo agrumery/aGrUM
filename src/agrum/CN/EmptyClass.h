916f79d3987a586534703f08f90542fe829ee2ec
Matthieu Hourbracq <matthieu.hourbracq@gmail.com>
2013-03-25 14:48:29 +0100

    forgot emptyClass (reallly empty) used as default value to allow variadic templates arguments

---
+++ src/agrum/CN/EmptyClass.h
@@ -0,0 +1,10 @@
+#ifndef __EMPTY_CLASS__H___
+#define __EMPTY_CLASS__H___
+
+  /**
+   * class used as default template argument to allow variadic templates without c++11(0x)
+   */
+  class EmptyClass {};
+
+#endif
+
