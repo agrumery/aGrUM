623332c303d02d93fa49f83dfa73646fcbf4aee3
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2013-04-26 14:34:44 +0200

    still...

--- src/agrum/CN/CNMonteCarloSampling.h
+++ src/agrum/CN/CNMonteCarloSampling.h
@@ -7,92 +7,92 @@
 namespace gum {
   namespace credal {
 
-  /**
-   * @class CNMonteCarloSampling CNMonteCarloSampling.h <agrum/CN/CNMonteCarloSampling.h>
-   * @brief Basic sampling algorithm (pure random).
-   * @tparam GUM_SCALAR A floating type ( float, double, long double ... ).
-   * @tparam BNInferenceEngine A BayesNet inference engine such as LazyPropagation.
-   */
-  template< typename GUM_SCALAR, class BNInferenceEngine >
-  class CNMonteCarloSampling : public MultipleInferenceEngine< GUM_SCALAR, BNInferenceEngine > {
-    private:
-      /** To easily acces MultipleInferenceEngine< GUM_SCALAR, BNInferenceEngine > methods. */
-      typedef MultipleInferenceEngine< GUM_SCALAR, BNInferenceEngine > infEs;
+    /**
+     * @class CNMonteCarloSampling CNMonteCarloSampling.h <agrum/CN/CNMonteCarloSampling.h>
+     * @brief Basic sampling algorithm (pure random).
+     * @tparam GUM_SCALAR A floating type ( float, double, long double ... ).
+     * @tparam BNInferenceEngine A BayesNet inference engine such as LazyPropagation.
+     */
+    template< typename GUM_SCALAR, class BNInferenceEngine >
+    class CNMonteCarloSampling : public MultipleInferenceEngine< GUM_SCALAR, BNInferenceEngine > {
+      private:
+        /** To easily acces MultipleInferenceEngine< GUM_SCALAR, BNInferenceEngine > methods. */
+        typedef MultipleInferenceEngine< GUM_SCALAR, BNInferenceEngine > infEs;
 
 //////////////////////////////////////////
-      /// @name Private initialization methods
+        /// @name Private initialization methods
 //////////////////////////////////////////
-      /// @{
-      /** Initialize approximation Scheme. */
-      void __mcInitApproximationScheme();
-      /** Initialize threads data. */
-      void __mcThreadDataCopy();
-      /// @}
-
-//////////////////////////////////////////
-      /// @name Private algorithm methods
-//////////////////////////////////////////
-      /// @{
-      /** Thread samples a BayesNet from the CredalNet. */
-      inline void __verticesSampling();
-
-      /** Insert CredalNet evidence into a thread BNInferenceEngine. */
-      inline void __insertEvidence();
-
-      /** Thread performs an inference using BNInferenceEngine. Calls __verticesSampling and __insertEvidence. */
-      inline void __threadInference();
-
-      /** Update thread data after a BayesNet inference. */
-      inline void __threadUpdate();
-
-      /**
-       * Get the binary representation of a given value.
-       * @param toFill A reference to the bits to fill. Size must be correct before passing argument (i.e. big enough to represent \c value)
-       * @param value The constant integer we want to binarize.
-       */
-      inline void __binaryRep ( std::vector< bool > & toFill,  const unsigned int value ) const;
-
-      /// @}
-
-
-    protected:
-
-    public:
-//////////////////////////////////////////
-      /// @name Constructors / Destructors
-//////////////////////////////////////////
-      /// @{
-      /**
-       * Constructor.
-       * @param credalNet The CredalNet to be used by the algorithm.
-       */
-      CNMonteCarloSampling ( const CredalNet< GUM_SCALAR > & credalNet );
-      /** Destructor. */
-      virtual ~CNMonteCarloSampling();
-      /// @}
+        /// @{
+        /** Initialize approximation Scheme. */
+        void __mcInitApproximationScheme();
+        /** Initialize threads data. */
+        void __mcThreadDataCopy();
+        /// @}
 
 //////////////////////////////////////////
-      /// @name Public algorithm methods
+        /// @name Private algorithm methods
 //////////////////////////////////////////
-      /// @{
+        /// @{
+        /** Thread samples a BayesNet from the CredalNet. */
+        inline void __verticesSampling();
 
-      /** Starts the inference. */
-      void makeInference();
+        /** Insert CredalNet evidence into a thread BNInferenceEngine. */
+        inline void __insertEvidence();
 
-      /// @}
+        /** Thread performs an inference using BNInferenceEngine. Calls __verticesSampling and __insertEvidence. */
+        inline void __threadInference();
 
-      /** Erase all inference data (including thread data). */
-      //void eraseAllEvidence();
+        /** Update thread data after a BayesNet inference. */
+        inline void __threadUpdate();
 
-      //// debug /////
+        /**
+         * Get the binary representation of a given value.
+         * @param toFill A reference to the bits to fill. Size must be correct before passing argument (i.e. big enough to represent \c value)
+         * @param value The constant integer we want to binarize.
+         */
+        inline void __binaryRep ( std::vector< bool > & toFill,  const unsigned int value ) const;
 
-      unsigned int notOptDelete;
+        /// @}
 
-      ////////////////
 
-    protected:
-      bool _repetitiveInd;
-  };
+      protected:
+
+      public:
+//////////////////////////////////////////
+        /// @name Constructors / Destructors
+//////////////////////////////////////////
+        /// @{
+        /**
+         * Constructor.
+         * @param credalNet The CredalNet to be used by the algorithm.
+         */
+        CNMonteCarloSampling ( const CredalNet< GUM_SCALAR > & credalNet );
+        /** Destructor. */
+        virtual ~CNMonteCarloSampling();
+        /// @}
+
+//////////////////////////////////////////
+        /// @name Public algorithm methods
+//////////////////////////////////////////
+        /// @{
+
+        /** Starts the inference. */
+        void makeInference();
+
+        /// @}
+
+        /** Erase all inference data (including thread data). */
+        //void eraseAllEvidence();
+
+        //// debug /////
+
+        unsigned int notOptDelete;
+
+        ////////////////
+
+      protected:
+        bool _repetitiveInd;
+    };
 
 
   } // namespace cn
