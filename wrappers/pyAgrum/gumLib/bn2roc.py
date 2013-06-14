f906483b2069ed906ad5fa087655e219a3e2ab9b
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2013-06-14 17:04:24 +0200

    updating samples and notebooks for pyAgrum and gumLib

--- wrappers/pyAgrum/gumLib/bn2roc.py
+++ wrappers/pyAgrum/gumLib/bn2roc.py
@@ -23,7 +23,6 @@
 
 import sys,os,csv
 
-
 from utils.progress_bar import ProgressBar
 from utils.pyAgrum_header import pyAgrum_header
 
@@ -57,7 +56,19 @@ def checkCompatibility(bn,fields,csv_name):
 
     return res
 
-def computeROCpoints(bn,csv_name,target,label,visible=False,transforme_label=None):
+
+def computeAUC(points):
+    """
+    Given a set of points drawing a ROC curve, compute the AUC value
+    """
+    #computes the integral from 0 to 1
+    somme=0
+    for i in range(1,len(points)):
+      somme+=(points[i-1][0]-points[i][0])*points[i][1]
+      
+    return -somme
+
+def computeROCpoints(bn,csv_name,target,label,visible=False):
     idTarget=bn.idFromName(target)
     engine=gum.LazyPropagation(bn)
 
@@ -114,39 +125,34 @@ def computeROCpoints(bn,csv_name,target,label,visible=False,transforme_label=Non
     if visible:
         print
     return (res,totalP,totalN,idTarget)
-
-def computeROC(bn,values,totalP,totalN,idTarget,label,transforme_label):
+    
+def computeROC(bn,values,totalP,totalN,idTarget,modalite):
     res=sorted(values,key=lambda x:x[0])
 
     vp=0.0
     fp=0.0
+    xopt=0.0
+    yopt=0.0
+    opt=100.0
+    seuilopt=0
     points=[(vp/totalP,fp/totalN)]
     for i in range(len(res)):
-        res_label=bn.variable(idTarget).label(int(res[i][1]))
-        if not transforme_label is None:
-          res_label=transforme_label(res_label)
-        if str(res_label)==str(label):
+        res_id=int(res[i][1])
+        if res_id==idTarget:
             vp+=1.0
         else:
             fp+=1.0
-        points.append((vp/totalP,fp/totalN))
+        x=vp/totalP
+        y=fp/totalN
+        d=x*x+(1-y)*(1-y)
+        if d<opt:
+          opt=d
+          xopt=x
+          yopt=y
+          seuilopt=res[i][0]
+        points.append((x,y))
 
-    return points
-
-def getNumLabel(inst,i,label,transforme_label):
-    if transforme_label is not None:
-        label=transforme_label(label)
-
-    if label.isdigit(): # an indice
-        return int(label)
-    else:
-        return inst.variable(i)[label]
-
-def add_state(label):
-    if label.isdigit():
-        return "State_"+str(int(label))
-    else:
-        return label
+    return points,(xopt,yopt),seuilopt
 
 def module_help(exit_value=1,message=""):
     """
@@ -158,47 +164,74 @@ def module_help(exit_value=1,message=""):
     print
     sys.exit(exit_value)
 
-def drawROC(points,zeLabel,zeFilename,visible,show_fig):
+def drawROC(points,zeTitle,zeFilename,visible,show_fig,save_fig=True,
+            special_point=None,special_value=None,special_label=None):
+    AUC=computeAUC(points)
+    
     import pylab
 
     pylab.clf()
+    pylab.grid(color='#aaaaaa', linestyle='-', linewidth=1,alpha=0.5)
+    
+    pylab.plot([x[0] for x in points], [y[1] for y in points], '-', linewidth=3,color="#000088",zorder=3)
+    pylab.plot([0.0,1.0], [0.0, 1.0], '-',color="#AAAAAA")
+    
     pylab.ylim((-0.01,1.01))
     pylab.xlim((-0.01,1.01))
     pylab.xticks(pylab.arange(0,1.1,.1))
     pylab.yticks(pylab.arange(0,1.1,.1))
     pylab.grid(True)
+    
+    ax=pylab.gca()
+    r = pylab.Rectangle((0,0), 1, 1, edgecolor='#444444', facecolor='none',zorder=1)
+    ax.add_patch(r)
+    [spine.set_visible(False) for spine in ax.spines.values()]
+    
+    pylab.xlabel('False positive rate')
+    pylab.ylabel('True positive rate')
+    
+    if special_point is not None:
+        pylab.plot(special_point[0],special_point[1],'o',color="#DD9999",zorder=6)
+        if special_value is not None:
+            pylab.text(special_point[0]+0.01,special_point[1]-0.01, special_value,
+                       {'color' : '#DD5555', 'fontsize' : 10},
+                       horizontalalignment = 'left',
+                       verticalalignment = 'top',
+                       rotation = 0,
+                       clip_on = False)
+    if special_label is not None:
+        if special_label!="":
+            labels=[special_label]
+            colors=['#DD9999']
+            circles=[pylab.Circle((0, 0), 1, fc=colors[0])]
+            legend_location = 'lower right'
+            pylab.legend(circles, labels, loc=legend_location)
 
-    pylab.plot([x[0] for x in points], [y[1] for y in points], '-', linewidth=1, label= zeLabel)
-    pylab.plot([0.0,1.0], [0.0, 1.0], 'k-')
+    pylab.text(0.5, 0.55,'AUC=%f'%AUC,
+     horizontalalignment='center',
+     verticalalignment='center',
+     rotation=39,
+     fontsize=18)
 
-    pylab.legend(loc='lower right')
-    if visible:
-        pylab.savefig(zeFilename)
-        #print ("result in "+zeFilename)
+    pylab.title(zeTitle)
+
+    if save_fig:
+        pylab.savefig(zeFilename,dpi=300)
+        print ("result in "+zeFilename)
 
     if show_fig:
         pylab.show()
 
-
-def showROC(bn,csv_name,variable,label,visible=True,show_fig=False,transforme_label=None):
-  (res,totalP,totalN,idTarget)=computeROCpoints(bn,csv_name,variable,label,visible,transforme_label)
-  points=computeROC(bn,res,totalP,totalN,idTarget,label,transforme_label)
-#  print points[0]
-#  print points[1]
-#  print points[2]
-#  print points[3]
-#  print "..."
-#  print points[len(points)-4]
-#  print points[len(points)-3]
-#  print points[len(points)-2]
-#  print points[len(points)-1]
+def showROC(bn,csv_name,variable,label,visible=True,show_fig=False):
+  (res,totalP,totalN,idTarget)=computeROCpoints(bn,csv_name,variable,label,visible)
+  points,opt,seuil=computeROC(bn,res,totalP,totalN,idTarget,label) 
 
   shortname=os.path.basename(bn.property("name"))
-  label=shortname+" - "+csv_name+ " - "+variable+"="+str(label)
+  title=shortname+" vs "+csv_name+ " - "+variable+"="+str(label)
 
   figname='roc_'+shortname+"-"+csv_name+ "-"+variable+"-"+str(label)+'.png'
 
-  drawROC(points,label,figname,visible,show_fig)
+  drawROC(points,title,figname,visible,show_fig,special_point=opt,special_value=seuil)
 
 def checkROCargs():
   pyAgrum_header("2011-13")
@@ -234,4 +267,4 @@ def checkROCargs():
 
 if __name__=="__main__":
   (bn,csv_name,variable,label)=checkROCargs()
-  showROC(bn,csv_name,variable,label,True,False,None)
+  showROC(bn,csv_name,variable,label,True,False)
