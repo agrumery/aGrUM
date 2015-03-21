e82879e2f59132cfe9f487b1802de05caff57ff6
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2015-03-21 22:12:28 +0100

    some updates for act (mac)

--- wrappers/pyAgrum/cmake/__init__.in.py
+++ wrappers/pyAgrum/cmake/__init__.in.py
@@ -56,7 +56,10 @@ from @PYAGRUM_MODULE@ import CredalNet,CNMonteCarloSampling,CNLoopyPropagation
 
 from @PYAGRUM_MODULE@ import DefaultInLabel,DuplicateElement,DuplicateLabel,EmptyBSTree,EmptySet,Exception,FatalError,FormatNotFound,GraphError,IOError,IdError,InvalidArc,InvalidArgument,InvalidArgumentsNumber,InvalidDirectedCycle,InvalidEdge,InvalidNode,NoChild,NoNeighbour,NoParent,NotFound,NullElement,OperationNotAllowed,OutOfBounds,OutOfLowerBound,OutOfUpperBound,ReferenceError,SizeError,SyntaxError,UndefinedElement,UndefinedIteratorKey,UndefinedIteratorValue
 
-## if needed
+# seed is chosen randomly :)
+initRandom(0)
+
+## if needed python2 vs python3 
 #import sys
 #if sys.version_info[0] == 2:
 #    from two import *
