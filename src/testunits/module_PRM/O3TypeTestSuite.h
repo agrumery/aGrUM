464f73ae236b854afa92ec5f2a6c6185f2aae3b7
Lionel Torti <lionel.torti@lip6.fr>
2016-01-23 15:40:06 +0100

    [prm] Added test for extended types.

--- src/testunits/module_PRM/O3TypeTestSuite.h
+++ src/testunits/module_PRM/O3TypeTestSuite.h
@@ -193,6 +193,30 @@ namespace gum_tests {
       TS_ASSERT_EQUALS( prm.types().size(), 1 );
       TS_ASSERT( not prm.isType( "state" ) );
     }
+
+    void testExtendedType() {
+      // Arrange
+      auto input = std::stringstream();
+      input << "type state extends boolean "
+            << "OK: true,"
+            << "NOK: false;";
+      auto output = std::stringstream();
+      gum::prm::PRM<double> prm;
+      // Act
+      TS_GUM_ASSERT_THROWS_NOTHING(
+          gum::prm::o3prm::parse_stream( prm, input, output ) );
+      // Assert
+      TS_ASSERT_EQUALS( output.str(), "" );
+      TS_ASSERT_EQUALS( prm.types().size(), 2 );
+      TS_ASSERT( prm.isType( "state" ) );
+      const auto& boolean = prm.type( "boolean" );
+      const auto& state = prm.type( "state" );
+      TS_ASSERT( state.isSubTypeOf( boolean ) );
+      const auto& map = state.label_map();
+      TS_ASSERT_EQUALS( map.size(), 2 );
+      TS_ASSERT_EQUALS( map[0], 0 );
+      TS_ASSERT_EQUALS( map[1], 1 );
+    }
   };
 
 }  // namespace gum_tests
