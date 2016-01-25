123f02d588ce981f6ed4140d6b829f24ff2f13d7
Lionel Torti <lionel.torti@lip6.fr>
2016-01-25 10:52:49 +0100

    [prm] Added precise location to type errors.

--- src/agrum/PRM/newo3prm/o3prm.cpp
+++ src/agrum/PRM/newo3prm/o3prm.cpp
@@ -62,6 +62,20 @@ namespace gum {
 
       Position::~Position() { GUM_DESTRUCTOR( Position ); }
 
+      Position& Position::operator=( const Position& src ) {
+        __file = src.__file;
+        __line = src.__line;
+        __column = src.__column;
+        return *this;
+      }
+
+      Position& Position::operator=( Position&& src ) {
+        __file = std::move( src.__file );
+        __line = std::move( src.__line );
+        __column = std::move( src.__column );
+        return *this;
+      }
+
       const std::string& Position::file() const { return __file; }
       void Position::file( const std::string& file ) { __file = file; }
 
@@ -71,17 +85,62 @@ namespace gum {
       int Position::column() const { return __column; }
       void Position::column( int column ) { __column = column; }
 
+      O3Label::O3Label()
+          : __pos()
+          , __label() {
+        GUM_CONSTRUCTOR( O3Label );
+      }
 
-      O3Type::O3Type( Position pos,
-                      std::string name,
-                      std::string super,
-                      O3Type::LabelMap& labels )
-          : __position( pos )
+      O3Label::O3Label( const Position& pos, const std::string& label )
+          : __pos( pos )
+          , __label( label ) {
+        GUM_CONSTRUCTOR( O3Label );
+      }
+
+      O3Label::O3Label( const O3Label& src )
+          : __pos( src.__pos )
+          , __label( src.__label ) {
+        GUM_CONS_CPY( O3Label );
+      }
+
+      O3Label::O3Label( O3Label&& src )
+          : __pos( std::move( src.__pos ) )
+          , __label( std::move( src.__label ) ) {
+        GUM_CONS_MOV( O3Label );
+      }
+
+      O3Label::~O3Label() { GUM_DESTRUCTOR( O3Label ); }
+
+      O3Label& O3Label::operator=( const O3Label& src ) {
+        __pos = src.__pos;
+        __label = src.__label;
+        return *this;
+      }
+
+      O3Label& O3Label::operator=( O3Label&& src ) {
+        __pos = std::move( src.__pos );
+        __label = std::move( src.__label );
+        return *this;
+      }
+
+      const Position& O3Label::position() const { return __pos; }
+
+      void O3Label::position( const Position& pos ) { __pos = pos; }
+
+      const std::string& O3Label::label() const { return __label; }
+
+      void O3Label::label( const std::string& label ) { __label = label; }
+
+      O3Type::O3Type( const Position& pos,
+          const O3Label& name,
+          const O3Label& super,
+          LabelMap& labels )
+        : __position( pos )
           , __name( name )
           , __super( super )
           , __labels( labels ) {
-        GUM_CONSTRUCTOR( O3Type );
-      }
+            GUM_CONSTRUCTOR( O3Type );
+          }
 
       O3Type::O3Type( const O3Type& src )
           : __position( src.__position )
@@ -101,8 +160,24 @@ namespace gum {
 
       O3Type::~O3Type() { GUM_DESTRUCTOR( O3Type ); }
 
-      const std::string& O3Type::name() const { return __name; }
-      const std::string& O3Type::super() const { return __super; }
+      O3Type& O3Type::operator=( const O3Type& src ) {
+        __position = src.__position;
+        __name = src.__name;
+        __super = src.__super;
+        __labels = src.__labels;
+        return *this;
+      }
+
+      O3Type& O3Type::operator=( O3Type&& src ) {
+        __position = std::move( src.__position );
+        __name = std::move( src.__name );
+        __super = std::move( src.__super );
+        __labels = std::move( src.__labels );
+        return *this;
+      }
+
+      const O3Label& O3Type::name() const { return __name; }
+      const O3Label& O3Type::super() const { return __super; }
       const O3Type::LabelMap& O3Type::labels() const { return __labels; }
       const Position& O3Type::position() const { return __position; }
 
@@ -119,6 +194,16 @@ namespace gum {
       }
 
       O3PRM::~O3PRM() { GUM_DESTRUCTOR( O3PRM ); }
+
+      O3PRM& O3PRM::operator=( const O3PRM& src ) {
+        __types = src.__types;
+        return *this;
+      }
+
+      O3PRM& O3PRM::operator=( O3PRM&& src ) {
+        __types = std::move( src.__types );
+        return *this;
+      }
 
       std::vector<O3Type>& O3PRM::types() { return __types; }
       const std::vector<O3Type>& O3PRM::types() const { return __types; }
