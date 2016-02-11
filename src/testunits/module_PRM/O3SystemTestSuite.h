cdcc8e3392c2527f5d5c98a7b5a8ab47a87dcce6
Lionel Torti <lionel.torti@lip6.fr>
2016-02-11 19:13:20 +0100

    [prm] Adding some tests for O3Class and O3System.

---
+++ src/testunits/module_PRM/O3SystemTestSuite.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Pierre-Henri WUILLEMIN et Christophe GONZALES   *
+ *   {prenom.nom}@lip6.fr                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include <iostream>
+#include <string>
+#include <cmath>
+
+#include <cxxtest/AgrumTestSuite.h>
+#include <cxxtest/testsuite_utils.h>
+
+#include <agrum/core/exceptions.h>
+#include <agrum/prm/PRM.h>
+#include <agrum/prm/newo3prm/o3prm.h>
+#include <agrum/prm/newo3prm/O3PRMFactory.h>
+
+namespace gum_tests {
+
+  class O3SystemTestSuite : public CxxTest::TestSuite {
+    public:
+    gum::prm::PRM<double>* prm;
+
+    void setUp() {
+      try {
+        auto path = GET_RESSOURCES_PATH( "o3prm/printers.o3prm" );
+        prm = new gum::prm::PRM<double>();
+        auto input = std::ifstream( path );
+        auto output = std::stringstream();
+        gum::prm::o3prm::parse_stream( *prm, input, output );
+        if ( output.str() != "" ) {
+          GUM_TRACE( output.str() );
+          GUM_ERROR( gum::FatalError, "corrupted ressource file" );
+        }
+      } catch ( gum::Exception& e ) {
+        GUM_TRACE_NEWLINE;
+        GUM_SHOWERROR( e );
+        throw e;
+      }
+    }
+
+    void tearDown() { delete prm; }
+
+    void testSimpleSystem() {
+      // Arrange
+      auto input = std::stringstream();
+      input << "System Foo { }";
+      auto output = std::stringstream();
+      // Act
+      TS_GUM_ASSERT_THROWS_NOTHING(
+          gum::prm::o3prm::parse_stream( *prm, input, output ) );
+      // Assert
+      TS_ASSERT_EQUALS( output.str(), "" );
+      TS_ASSERT_EQUALS( prm->systems().size(), 1 );
+      TS_ASSERT( prm->isSystem( "Foo" ) );
+      const auto& foo = prm->system( "Foo" );
+      TS_ASSERT_EQUALS( foo.size(), 0 );
+    }
+  };
+
+}  // namespace gum_tests
+
