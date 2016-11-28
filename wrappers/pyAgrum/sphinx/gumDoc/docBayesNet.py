e03f0057c37aeaece0b26dd76bdf7dd2d97aa64b
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2016-11-28 16:11:13 +0100

    [pyAgrum] sphinx configuration files

---
+++ wrappers/pyAgrum/sphinx/gumDoc/docBayesNet.py
@@ -0,0 +1,5 @@
+import pyAgrum as gum
+
+gum.BayesNet.__doc___ = """
+BayesNet are blah blah blah
+"""
---
+++ wrappers/pyAgrum/sphinx/gumDoc/docEdge.py
@@ -0,0 +1,5 @@
+import pyAgrum
+
+pyAgrum.Edge.__doc__ = """
+pyAgrum.Edge blah blah blah
+"""
