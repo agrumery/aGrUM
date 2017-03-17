05751c21da565fb9308a47ac3db33eefa66d39c6
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2017-03-17 21:51:46 +0100

    [pyAgrum] allowing notebook graphs for BN outside notebook

--- wrappers/pyAgrum/lib/bn2graph.py
+++ wrappers/pyAgrum/lib/bn2graph.py
@@ -23,21 +23,259 @@
 
 from __future__ import print_function
 
-import os
-import subprocess
-import sys
+import time
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pyAgrum as gum
+import pydotplus as dot
+import shutil
 
-from ._utils.pyAgrum_header import pyAgrum_header
+
+def _proba2rgb(p=0.99, cmap=None):
+  if cmap is None:
+    cmap=plt.get_cmap('summer')
+
+  if p == 0.0:
+    r, g, b = "FF", "33", "33"
+  elif p == 1.0:
+    r, g, b = "AA", "FF", "FF"
+  else:
+    (r, g, b, _) = cmap(p)
+    r = "%02x" % int(r * 256)
+    g = "%02x" % int(g * 256)
+    b = "%02x" % int(b * 256)
+
+  return r, g, b
 
 
-def dotize(aBN, name, style='pdf'):
+def _proba2bgcolor(p, cmap=None):
+  if cmap is None:
+    cmap=plt.get_cmap('summer')
+
+  r, g, b = _proba2rgb(p, cmap)
+  return "#" + r + g + b
+
+
+def _proba2fgcolor(p, cmap=None):
+  if cmap is None:
+    cmap=plt.get_cmap('summer')
+
+  if max([eval("0x" + s[0]) for s in _proba2rgb(p, cmap)]) <= 12:
+    return "#FFFFFF"
+  else:
+    return "#000000"
+
+
+def BN2dot(bn, size="4", arcvals=None, vals=None, cmap=None, showValues=None):
+  if cmap is None:
+    cmap=plt.get_cmap('summer')
+  """
+  Shows a graphviz svg representation of the BN using size ("1" ,"2" , ...)
+  vals is a dictionnary name:value of value in [0,1] for each node
+  (with special color for 0 and 1)
+  """
+  if arcvals is not None:
+    minarcs = min(arcvals.values())
+    maxarcs = max(arcvals.values())
+
+  graph = dot.Dot(graph_type='digraph')
+  for n in bn.names():
+    if vals is None or n not in vals:
+      bgcol = "#444444"
+      fgcol = "#FFFFFF"
+      res = ""
+    else:
+      bgcol = _proba2bgcolor(vals[n], cmap)
+      fgcol = _proba2fgcolor(vals[n], cmap)
+      res = " : {0:2.5f}".format(vals[n] if showValues is None else showValues[n])
+
+    node = dot.Node('"' + n + '"', style="filled",
+                    fillcolor=bgcol,
+                    fontcolor=fgcol,
+                    tooltip='"({0}) {1}{2}"'.format(bn.idFromName(n), n, res))
+    graph.add_node(node)
+
+  for a in bn.arcs():
+    if arcvals is None:
+      pw = 1
+      av = ""
+    else:
+      if a in arcvals:
+        pw = 0.1 + 5 * (arcvals[a] - minarcs) / (maxarcs - minarcs)
+        av = arcvals[a]
+      else:
+        pw = 1
+
+    edge = dot.Edge('"' + bn.variable(a[0]).name() + '"', '"' + bn.variable(a[1]).name() + '"',
+                    penwidth=pw,
+                    tooltip="{} : {}".format(a, av))
+    graph.add_edge(edge)
+
+  graph.set_size(size)
+  return graph
+
+
+def _getProbaV(p):
+  """
+  compute the representation of an histogram for a mono-dim Potential
+
+  :param p: the mono-dim Potential
+  :return: a matplotlib bar (vertical histogram) for a Potential p.
+
+  """
+  var = p.variable(0)
+  ra = np.arange(var.domainSize())
+
+  fig = plt.figure()
+  fig.set_figwidth(var.domainSize() / 4.0)
+  fig.set_figheight(2)
+
+  ax = fig.add_subplot(111)
+
+  ax.bar(ra, p.tolist(), align='center')
+  # ax.set_ylim(0,1)
+  ax.set_xticks(ra)
+  ax.set_xticklabels([])
+  # ax.set_xlabel('Probability')
+  ax.set_title(var.name())
+  ax.get_yaxis().grid(True)
+  return fig
+
+
+def _getProbaH(p):
+  """
+  compute the representation of an histogram for a mono-dim Potential
+
+  :param p: the mono-dim Potential
+  :return: a matplotlib barh (horizontal histogram) for a Potential p.
+  """
+  var = p.variable(0)
+  ra = np.arange(var.domainSize())
+  ra_reverse = np.arange(var.domainSize() - 1, -1, -1)  # reverse order
+  vx = ["{0}:{1:1.4f}".format(var.label(int(i)), p[i]) for i in ra_reverse]
+
+  fig = plt.figure()
+  fig.set_figheight(var.domainSize() / 4.0)
+  fig.set_figwidth(2)
+
+  ax = fig.add_subplot(111)
+
+  vals = p.tolist()
+  vals.reverse()
+  ax.barh(ra, vals, align='center')
+  ax.set_xlim(0, 1)
+  ax.set_yticks(np.arange(var.domainSize()))
+  ax.set_yticklabels(vx)
+  ax.set_xticklabels([])
+  # ax.set_xlabel('Probability')
+  ax.set_title(var.name())
+  ax.get_xaxis().grid(True)
+  return fig
+
+
+def proba2histo(p):
+  """
+  compute the representation of an histogram for a mono-dim Potential
+
+  :param p: the mono-dim Potential
+  :return: a matplotlib histogram for a Potential p.
+  """
+  if p.variable(0).domainSize() > 8:
+    return _getProbaV(p)
+  else:
+    return _getProbaH(p)
+
+
+def _saveFigProba(p, filename, format="svg"):
+  fig = proba2histo(p)
+  fig.savefig(filename, bbox_inches='tight', transparent=True, pad_inches=0, dpi=fig.dpi, format=format)
+  plt.close(fig)
+
+
+def BNinference2dot(bn, engine=None, evs={}, targets={}, format='png', vals=None, arcvals=None,cmap=None):
+  """
+  repr a pydot graph for an inference in a notebook
+
+  :param gum.BayesNet bn:
+  :param gum.Inference engine: inference algorithm used. If None, LazyPropagation will be used
+  :param dictionnary evs: map of evidence
+  :param set targets: set of targets. If targets={} then each node is a target
+  :param string size: size of the rendered graph
+  :param string format: render as "png" or "svg"
+  :param boolean asString: display the graph or return a string containing the corresponding HTML fragment
+  :return: the desired representation of the inference
+  """
+  if cmap is None:
+    cmap=plt.get_cmap('summer')
+
+  startTime = time.time()
+  if engine is None:
+    ie = gum.LazyPropagation(bn)
+  else:
+    ie = engine
+  ie.setEvidence(evs)
+  ie.makeInference()
+  stopTime = time.time()
+
+  from tempfile import mkdtemp
+  temp_dir = mkdtemp("", "tmp", None)  # with TemporaryDirectory() as temp_dir:
+
+  dotstr = "digraph structs {\n"
+  dotstr += "  label=\"Inference in {:6.2f}ms\";\n".format(1000 * (stopTime - startTime))
+  dotstr += "  node [fillcolor=floralwhite, style=filled,color=grey];\n"
+
+  for n in bn.ids():
+    name = bn.variable(n).name()
+
+    if vals is None or name not in vals:
+      bgcol = "sandybrown" if name in evs else "#FFFFFF"
+      fgcol = "#000000"
+    else:
+      bgcol = _proba2bgcolor(vals[name], cmap)
+      fgcol = _proba2fgcolor(vals[name], cmap)
+    colorattribute = 'fillcolor="{}", fontcolor="{}", color="#000000"'.format(bgcol, fgcol)
+
+    if len(targets) == 0 or name in targets:
+      filename = temp_dir + name + "." + format
+      _saveFigProba(ie.posterior(n), filename, format=format)
+      fill = ", " + colorattribute
+      dotstr += ' "{0}" [shape=rectangle,image="{1}",label="" {2}];\n'.format(name, filename, fill)
+    else:
+      dotstr += ' "{0}" [{1}]'.format(name, colorattribute)
+
+  if arcvals is not None:
+    minarcs = min(arcvals.values())
+    maxarcs = max(arcvals.values())
+
+  for a in bn.arcs():
+    (n, j) = a
+    if arcvals is None:
+      pw = 1
+      av = ""
+    else:
+      if (n, j) in arcvals:
+        pw = 0.1 + 5 * (arcvals[a] - minarcs) / (maxarcs - minarcs)
+        av = arcvals[a]
+      else:
+        pw = 1
+
+    dotstr += ' "{0}"->"{1}" [penwidth={2},tooltip="{3}:{4}"];'.format(bn.variable(n).name(), bn.variable(j).name(),
+                                                                       pw, a, av)
+  dotstr += '}'
+
+  g = dot.graph_from_dot_data(dotstr)
+
+  shutil.rmtree(temp_dir)
+  return g
+
+
+def dotize(aBN, name, format='pdf'):
   """
   From a bn 'bn' and a name 'bn', ize creates 'bn.dot' and 'bn.style', representation of the bn in
   dot format and in style. style in [pdf,png,fig,jpg,svg]
   """
-  if style not in ['pdf', 'png', 'fig', 'jpg', 'svg']:
+  if format not in ['pdf', 'png', 'fig', 'jpg', 'svg']:
     raise Exception("<%s> in not a correct style ([pdf,png,fig,jpg,svg])" % style)
 
   if isinstance(aBN, str):
@@ -45,15 +283,8 @@ def dotize(aBN, name, style='pdf'):
   else:
     bn = aBN
 
-  dotfile = name + '.dot'
-  pngfile = name + '.' + style
-
-  f = open(dotfile, 'w')
-  f.write(bn.toDot())
-  f.close()
-
-  cmd = ['dot', '-T' + style, dotfile, ('-o' + pngfile)]
-  return subprocess.call(cmd)
+  imgfile = name + '.' + format
+  BN2dot(bn).write(imgfile, format=format)
 
 
 def pngize(aBN, name):
@@ -73,10 +304,17 @@ def pdfize(aBN, name):
 
 
 if __name__ == "__main__":
-  pyAgrum_header("2011-17")
-
-  if len(sys.argv) < 2:
-    print(os.path.basename(sys.argv[0]), "file.{" + gum.availableBNExts() + "}")
-  else:
-    base, ext = os.path.splitext(sys.argv[1])
-    pngize(sys.argv[1], base)
+  #  pyAgrum_header("2011-17")##
+  #
+  #  if len(sys.argv) < 2:
+  #    print(os.path.basename(sys.argv[0]), "file.{" + gum.availableBNExts() + "}")
+  #  else:
+  #    base, ext = os.path.splitext(sys.argv[1])
+  #    pngize(sys.argv[1], base)
+  bn = gum.fastBN("a->b->d;a->c->d->e;f->b")
+  g = BNinference2dot(bn,
+                      targets=['f', 'd'],
+                      vals={'a': 1, 'b': 0.3, 'c': 0.3, 'd': 0.1, 'e': 0.1, 'f': 0.3},
+                      arcvals={(0, 1): 2, (0, 2): 0.5})
+  g.write("test.pdf", format='pdf')
+  # pdfize(bn, "test")
--- wrappers/pyAgrum/lib/notebook.py
+++ wrappers/pyAgrum/lib/notebook.py
@@ -24,11 +24,10 @@ tools for BN analysis in jupyter notebook (but can be used everywhere)
 """
 from __future__ import print_function
 
-import base64
-import shutil
 import time
 
 import IPython.display
+import base64
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -38,7 +37,18 @@ from IPython.core.display import Image, display_png
 from IPython.core.pylabtools import print_figure
 from IPython.display import display, HTML, SVG
 from matplotlib.backends.backend_agg import FigureCanvasAgg as fc
+from pyAgrum.lib.bn2graph import BN2dot, proba2histo, BNinference2dot
 
+
+_cdict = {
+  'red'  : ((0.0, 0.1, 0.3),
+            (1.0, 0.6, 1.0)),
+  'green': ((0.0, 0.0, 0.0),
+            (1.0, 0.6, 0.8)),
+  'blue' : ((0.0, 0.0, 0.0),
+            (1.0, 1, 0.8))
+}
+_INFOcmap = mpl.colors.LinearSegmentedColormap('my_colormap', _cdict, 256)
 
 def configuration():
   """
@@ -61,7 +71,7 @@ def configuration():
     res += "<tr><td>%s</td><td>%s</td></tr>" % (name, packages[name])
 
   res += "</table><div align='right'><small>%s</small></div>" % time.strftime(
-    '%a %b %d %H:%M:%S %Y %Z')
+      '%a %b %d %H:%M:%S %Y %Z')
 
   return IPython.display.HTML(res)
 
@@ -197,77 +207,6 @@ def getInfluenceDiagram(diag, size="4", format="png"):
   return getDot(diag.toDot(), size, format)
 
 
-def _getProbaV(p):
-  """
-  compute the representation of an histogram for a mono-dim Potential
-
-  :param p: the mono-dim Potential
-  :return: a matplotlib bar (vertical histogram) for a Potential p.
-
-  """
-  var = p.variable(0)
-  ra = np.arange(var.domainSize())
-
-  fig = plt.figure()
-  fig.set_figwidth(var.domainSize() / 4.0)
-  fig.set_figheight(2)
-
-  ax = fig.add_subplot(111)
-
-  ax.bar(ra, p.tolist(), align='center')
-  # ax.set_ylim(0,1)
-  ax.set_xticks(ra)
-  ax.set_xticklabels([])
-  # ax.set_xlabel('Probability')
-  ax.set_title(var.name())
-  ax.get_yaxis().grid(True)
-  return fig
-
-
-def _getProbaH(p):
-  """
-  compute the representation of an histogram for a mono-dim Potential
-
-  :param p: the mono-dim Potential
-  :return: a matplotlib barh (horizontal histogram) for a Potential p.
-  """
-  var = p.variable(0)
-  ra = np.arange(var.domainSize())
-  ra_reverse = np.arange(var.domainSize() - 1, -1, -1)  # reverse order
-  vx = ["{0}:{1:1.4f}".format(var.label(int(i)), p[i]) for i in ra_reverse]
-
-  fig = plt.figure()
-  fig.set_figheight(var.domainSize() / 4.0)
-  fig.set_figwidth(2)
-
-  ax = fig.add_subplot(111)
-
-  vals = p.tolist()
-  vals.reverse()
-  ax.barh(ra, vals, align='center')
-  ax.set_xlim(0, 1)
-  ax.set_yticks(np.arange(var.domainSize()))
-  ax.set_yticklabels(vx)
-  ax.set_xticklabels([])
-  # ax.set_xlabel('Probability')
-  ax.set_title(var.name())
-  ax.get_xaxis().grid(True)
-  return fig
-
-
-def getProba(p):
-  """
-  compute the representation of an histogram for a mono-dim Potential
-
-  :param p: the mono-dim Potential
-  :return: a matplotlib histogram for a Potential p.
-  """
-  if p.variable(0).domainSize() > 8:
-    return _getProbaV(p)
-  else:
-    return _getProbaH(p)
-
-
 def showProba(p):
   """
   Show a mono-dim Potential
@@ -275,12 +214,12 @@ def showProba(p):
   :param p: the mono-dim Potential
   :return:
   """
-  fig = getProba(p)
+  fig = proba2histo(p)
   plt.show()
 
 
 def _saveFigProba(p, filename, format="svg"):
-  fig = getProba(p)
+  fig = proba2histo(p)
   fig.savefig(filename, bbox_inches='tight', transparent=True, pad_inches=0, dpi=fig.dpi, format=format)
   plt.close(fig)
 
@@ -297,7 +236,7 @@ def getPosterior(bn, evs, target):
   :type target: str
   :return: the matplotlib graph
   """
-  return getProba(gum.getPosterior(bn, evs=evs, target=target))
+  return proba2histo(gum.getPosterior(bn, evs=evs, target=target))
 
 
 def showPosterior(bn, evs, target):
@@ -328,8 +267,8 @@ def animApproximationScheme(apsc, scale=np.log10):
   def stopper(x):
     clear_output(True)
     plt.title(
-      "{0} \n Time : {1} s | Iterations : {2} | Espilon : {3}".format(x, apsc.currentTime(), apsc.nbrIterations(),
-                                                                      apsc.epsilon()))
+        "{0} \n Time : {1} s | Iterations : {2} | Espilon : {3}".format(x, apsc.currentTime(), apsc.nbrIterations(),
+                                                                        apsc.epsilon()))
 
   def progresser(x, y, z):
     if len(apsc.history()) < 10:
@@ -344,84 +283,7 @@ def animApproximationScheme(apsc, scale=np.log10):
   h.setWhenProgress(progresser)
 
 
-_cdict = {'red': ((0.0, 0.1, 0.3),
-                  (1.0, 0.6, 1.0)),
-          'green': ((0.0, 0.0, 0.0),
-                    (1.0, 0.6, 0.8)),
-          'blue': ((0.0, 0.0, 0.0),
-                   (1.0, 1, 0.8))}
-_INFOcmap = mpl.colors.LinearSegmentedColormap('my_colormap', _cdict, 256)
-
-
-def _proba2rgb(p=0.99, cmap=_INFOcmap):
-  if p == 0.0:
-    r, g, b = "FF", "33", "33"
-  elif p == 1.0:
-    r, g, b = "AA", "FF", "FF"
-  else:
-    (r, g, b, _) = cmap(p)
-    r = "%02x" % int(r * 256)
-    g = "%02x" % int(g * 256)
-    b = "%02x" % int(b * 256)
-
-  return r, g, b
-
-
-def _proba2bgcolor(p, cmap=_INFOcmap):
-  r, g, b = _proba2rgb(p, cmap)
-  return "#" + r + g + b
-
-
-def _proba2fgcolor(p, cmap=_INFOcmap):
-  if max([eval("0x" + s[0]) for s in _proba2rgb(p, cmap)]) <= 12:
-    return "#FFFFFF"
-  else:
-    return "#000000"
-
-
-def _BN2colouredDot(bn, size="4", arcvals=None, vals=None, cmap=_INFOcmap,showValues=None):
-  """
-  Shows a graphviz svg representation of the BN using size ("1" ,"2" , ...)
-  vals is a dictionnary name:value of value in [0,1] for each node
-  (with special color for 0 and 1)
-  """
-  if arcvals is not None:
-    minarcs = min(arcvals.values())
-    maxarcs = max(arcvals.values())
-
-  graph = dot.Dot(graph_type='digraph')
-  for n in bn.names():
-    if vals is None or n not in vals:
-      bgcol = "#444444"
-      fgcol = "#FFFFFF"
-      res = ""
-    else:
-      bgcol = _proba2bgcolor(vals[n], cmap)
-      fgcol = _proba2fgcolor(vals[n], cmap)
-      res = " : {0:2.5f}".format(vals[n] if showValues is None else showValues[n])
-
-    node = dot.Node('"' + n + '"', style="filled",
-                    fillcolor=bgcol,
-                    fontcolor=fgcol,
-                    tooltip='"({0}) {1}{2}"'.format(bn.idFromName(n), n, res))
-    graph.add_node(node)
-  for a in bn.arcs():
-    if arcvals is None:
-      pw = 1
-      av = ""
-    else:
-      pw = 0.1 + 5 * (arcvals[a] - minarcs) / (maxarcs - minarcs)
-      av = arcvals[a]
-
-    edge = dot.Edge('"' + bn.variable(a[0]).name() + '"', '"' + bn.variable(a[1]).name() + '"',
-                    penwidth=pw,
-                    tooltip="{} : {}".format(a, av))
-    graph.add_edge(edge)
-  graph.set_size(size)
-  return graph
-
-
-def showBN(bn, size="4", format="svg", arcvals=None, vals=None, cmap=_INFOcmap, asString=False):
+def showBN(bn, size="4", format="svg", arcvals=None, vals=None, cmap=None):
   """
   show a Bayesian network
 
@@ -433,10 +295,10 @@ def showBN(bn, size="4", format="svg", arcvals=None, vals=None, cmap=_INFOcmap, 
   :param cmap: color map to show the vals
   :return: the graph
   """
-  return showGraph(_BN2colouredDot(bn, size, arcvals, vals, cmap), size, format)
+  return showGraph(BN2dot(bn, size, arcvals, vals, cmap), size, format)
 
 
-def getBN(bn, size="4", format="svg", arcvals=None, vals=None, cmap=_INFOcmap, asString=False):
+def getBN(bn, size="4", format="svg", arcvals=None, vals=None, cmap=None):
   """
   get a HTML string for a Bayesian network
 
@@ -448,7 +310,7 @@ def getBN(bn, size="4", format="svg", arcvals=None, vals=None, cmap=_INFOcmap, a
   :param cmap: color map to show the vals
   :return: the graph
   """
-  return getGraph(_BN2colouredDot(bn, size, arcvals, vals, cmap), size, format)
+  return getGraph(BN2dot(bn, size, arcvals, vals, cmap), size, format)
 
 
 def _normalizeVals(vals, hilightExtrema=False):
@@ -492,7 +354,7 @@ def _reprInformation(bn, evs, size, cmap, asString):
   idEvs = {bn.idFromName(name) for name in evs}
   nodevals = {bn.variable(n).name(): ie.H(n) for n in bn.ids() if not n in idEvs}
   arcvals = {(x, y): ie.I(x, y) for x, y in bn.arcs()}
-  gr = _BN2colouredDot(bn, size, arcvals, _normalizeVals(nodevals, hilightExtrema=False), cmap,showValues=nodevals)
+  gr = _BN2colouredDot(bn, size, arcvals, _normalizeVals(nodevals, hilightExtrema=False), cmap, showValues=nodevals)
 
   mi = min(nodevals.values())
   ma = max(nodevals.values())
@@ -507,7 +369,7 @@ def _reprInformation(bn, evs, size, cmap, asString):
   cb1.set_label('Entropy')
   png = print_figure(canvas.figure, "png")  # from IPython.core.pylabtools
   png_legend = "<img style='vertical-align:middle' src='data:image/png;base64,%s'>" % base64.encodestring(png).decode(
-    'ascii')
+      'ascii')
 
   gsvg = SVG(gr.create_svg())
 
@@ -550,57 +412,7 @@ def showInformation(bn, evs={}, size="4", cmap=_INFOcmap):
   return _reprInformation(bn, evs, size, cmap, asString=False)
 
 
-def _reprInference(bn, engine, evs, targets, size, format, asString):
-  """
-  repr a pydot graph for an inference in a notebook
-
-  :param gum.BayesNet bn:
-  :param gum.Inference engine: inference algorithm used. If None, LazyPropagation will be used
-  :param dictionnary evs: map of evidence
-  :param set targets: set of targets. If targets={} then each node is a target
-  :param string size: size of the rendered graph
-  :param string format: render as "png" or "svg"
-  :param boolean asString: display the graph or return a string containing the corresponding HTML fragment
-  :return: the desired representation of the inference
-  """
-  startTime = time.time()
-  if engine is None:
-    ie = gum.LazyPropagation(bn)
-  else:
-    ie = engine
-  ie.setEvidence(evs)
-  ie.makeInference()
-  stopTime = time.time()
-
-  from tempfile import mkdtemp
-  temp_dir = mkdtemp("", "tmp", None)  # with TemporaryDirectory() as temp_dir:
-
-  dotstr = "digraph structs {\n"
-  dotstr += "  label=\"Inference in {:6.2f}ms\";\n".format(1000 * (stopTime - startTime))
-  dotstr += "  node [fillcolor=floralwhite, style=filled,color=grey];\n"
-
-  for i in bn.ids():
-    name = bn.variable(i).name()
-    if len(targets) == 0 or name in targets:
-      filename = temp_dir + name + "." + format
-      _saveFigProba(ie.posterior(i), filename, format=format)
-      fill = ", fillcolor=sandybrown" if name in evs else ""
-      dotstr += ' "{0}" [shape=rectangle,image="{1}",label="" {2}];\n'.format(name, filename, fill)
-    else:
-      fill = "[fillcolor=sandybrown]" if name in evs else ""
-      dotstr += ' "{0}" {1}'.format(name, fill)
-  for (i, j) in bn.arcs():
-    dotstr += ' "{0}"->"{1}";'.format(bn.variable(i).name(), bn.variable(j).name())
-  dotstr += '}'
-
-  g = dot.graph_from_dot_data(dotstr)
-  sss = _reprGraph(g, size=size, format=format, asString=asString)
-
-  shutil.rmtree(temp_dir)
-  return sss
-
-
-def showInference(bn, engine=None, evs={}, targets={}, size="7", format='png'):
+def showInference(bn, engine=None, evs={}, targets={}, size="7", format='png', vals=None, arcvals=None, cmap=None):
   """
   show pydot graph for an inference in a notebook
 
@@ -613,10 +425,10 @@ def showInference(bn, engine=None, evs={}, targets={}, size="7", format='png'):
   :param boolean asString: display the graph or return a string containing the corresponding HTML fragment
   :return: the desired representation of the inference
   """
-  return _reprInference(bn, engine, evs, targets, size, format, asString=False)
+  return showGraph(BNinference2dot(bn, engine, evs, targets, format, vals, arcvals, cmap), size, format)
 
 
-def getInference(bn, engine=None, evs={}, targets={}, size="7", format='png'):
+def getInference(bn, engine=None, evs={}, targets={}, size="7", format='png', vals=None, arcvals=None, cmap=None):
   """
   get a HTML string for an inference in a notebook
 
@@ -629,7 +441,7 @@ def getInference(bn, engine=None, evs={}, targets={}, size="7", format='png'):
   :param boolean asString: display the graph or return a string containing the corresponding HTML fragment
   :return: the desired representation of the inference
   """
-  return _reprInference(bn, engine, evs, targets, size, format, asString=True)
+  return getGraph(BNinference2dot(bn, engine, evs, targets, format, vals, arcvals, cmap), size, format)
 
 
 def _reprPotential(pot, digits=4, withColors=True, varnames=None, asString=False):
@@ -673,7 +485,7 @@ def _reprPotential(pot, digits=4, withColors=True, varnames=None, asString=False
   else:
     if varnames is not None and len(varnames) != pot.nbrDim():
       raise ValueError(
-        "varnames contains {} values instead of the needed {} values.".format(len(varnames), pot.nbrDim()))
+          "varnames contains {} values instead of the needed {} values.".format(len(varnames), pot.nbrDim()))
 
     nparents = pot.nbrDim() - 1
     var = pot.variable(0)
@@ -682,13 +494,14 @@ def _reprPotential(pot, digits=4, withColors=True, varnames=None, asString=False
     # first line
     if nparents > 0:
       html.append(
-        "<tr><th colspan='{}'></th><th colspan='{}' style='background-color:#AAAAAA'><center>{}</center></th></tr>".format(
-          nparents, var.domainSize(), varname))
+          "<tr><th colspan='{}'></th><th colspan='{}' style='background-color:#AAAAAA'><center>{"
+          "}</center></th></tr>".format(
+              nparents, var.domainSize(), varname))
     else:
       html.append(
-        "<tr style='background-color:#AAAAAA'><th colspan='{}'><center>{}</center></th></tr>".format(
-          var.domainSize(),
-          varname))
+          "<tr style='background-color:#AAAAAA'><th colspan='{}'><center>{}</center></th></tr>".format(
+              var.domainSize(),
+              varname))
     # second line
     s = "<tr>"
     if nparents > 0:
@@ -718,7 +531,8 @@ def _reprPotential(pot, digits=4, withColors=True, varnames=None, asString=False
           s += "<th style='background-color:#BBBBBB'><center>{}</center></th>".format(label)
         else:
           if sum([inst.val(i) for i in range(1, par)]) == 0:
-            s += "<th style='background-color:#BBBBBB;' rowspan = '{}'><center>{}</center></th>".format(offset[par], label)
+            s += "<th style='background-color:#BBBBBB;' rowspan = '{}'><center>{}</center></th>".format(offset[par],
+                                                                                                        label)
       for j in range(pot.variable(0).domainSize()):
         s += _mkCell(pot.get(inst))
         inst.inc()
@@ -806,7 +620,7 @@ def getSideBySide(*args, **kwargs):
   if captions is not None:
     s += '<tr><td style="border-top:hidden;border-bottom:hidden;"><div align="center"><small>'
     s += '</small></div></td><td style="border-top:hidden;border-bottom:hidden;"><div align="center"><small>'.join(
-      captions)
+        captions)
     s += '</small></div></td></tr>'
 
   s += '</table>'
