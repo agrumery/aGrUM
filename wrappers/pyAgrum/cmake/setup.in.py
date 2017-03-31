4ca0caa06ff4945badb26a0eb0e30744df55f30a
Lionel <lionel.torti@gmail.com>
2017-03-31 09:06:11 +0000

    Internal/wheels metafiles

---
+++ wrappers/pyAgrum/cmake/setup.in.py
@@ -0,0 +1,78 @@
+"""pyAgrum setup file
+See:
+https://pyagrum.lip6.fr
+"""
+# Always prefer setuptools over distutils
+try:
+  from setuptools import setup, Extension
+except ImportError:
+  from distutils.core import setup, Extension
+
+from io import open
+
+README = ''
+with open('README.rst', mode='r', encoding='utf-8') as fd:
+  README = fd.read()
+
+swig_files = [ "pyAgrum/pyAgrum.i" ]
+flags = ["-fopenmp",
+         "-std=c++1y", 
+         "-Wall", 
+         "-pedantic", 
+         "-fopenmp", 
+         "-fPIC"]
+
+options = ["-c++",
+           "-I/usr/loca/include",
+           "-I/usr/include",
+           "-I./pyAgrum/extensions"]
+
+my_ext = Extension( name='_pyAgrum',
+    sources=swig_files,
+    include_dirs=["pyAgrum/extensions", "/usr/include"],
+    define_macros=[("NDEBUG", None)],
+    extra_compile_args=flags,
+    language="c++",
+    libraries=['agrum'],
+    swig_opts=options
+    )
+
+setup(
+    name='pyAgrum',
+    version='@PYAGRUM_VERSION@',
+    description='pyAgrum is a Python wrapper for the C++ aGrUM library',
+    url='https://pyagrum.lip6.fr',
+    maintainer='Pierre-Henri Wuillemin and Christophe Gonzales',
+    maintainer_email='agrum@lip6.fr',
+    platforms=['any'],
+    license='GPL',
+    packages=['pyAgrum', 'pyAgrum/lib', 'pyAgrum/lib/_utils'],
+    long_description=README,
+    include_package_data=True,
+    ext_modules=[my_ext],
+    install_requires=['numpy'],
+    classifiers=[
+    'Development Status :: 4 - Beta', 
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: MacOS :: MacOS X', 
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: C++',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Financial and Insurance Industry',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    ],
+    keywords='probabilities probabilistic-graphical-models inference diagnosis',
+    )
---
+++ wrappers/pyAgrum/wheelhouse/build_wheels.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -e -x
+
+# Setting up paths to where everything is/will be
+AGRUM_DIR=/opt/aGrUM
+PYAGRUM_DIR=/opt/aGrUM/wrappers/pyAgrum
+WHEELHOUSE_DIR=/opt/wheelhouse
+
+# Downloading aGrUM and initializing the wheelhouse dir
+git clone https://gitlab.com/agrumery/aGrUM.git ${AGRUM_DIR}
+mkdir -p ${WHEELHOUSE_DIR}/pyAgrum
+cp -R ${PYAGRUM_DIR} ${WHEELHOUSE_DIR}/pyAgrum/
+cp ${AGRUM_DIR}/wrappers/swig/* ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum
+
+# Extracting pyAgrum version from VERSION.txt
+MAJOR=$(cat ${AGRUM_DIR}/VERSION.txt | grep AGRUM_VERSION_MAJOR | sed 's/set(AGRUM_VERSION_MAJOR "\(.*\)")/\1/')
+MINOR=$(cat ${AGRUM_DIR}/VERSION.txt | grep AGRUM_VERSION_MINOR | sed 's/set(AGRUM_VERSION_MINOR "\(.*\)")/\1/')
+PATCH=$(cat ~/Projects/aGrUM/VERSION.txt | grep 'set(AGRUM_VERSION_PATCH' | sed 's/set(AGRUM_VERSION_PATCH "\([0-9]*\)\.[0-9]*")/\1/')
+WHEEL_PATCH=$(cat ${AGRUM_DIR}/VERSION.txt | grep PYAGRUM_VERSION_PATCH | sed 's/set(PYAGRUM_VERSION_PATCH "\(.*\)")/\1/')
+
+PYAGRUM_VERSION="${MAJOR}.${MINOR}.${PATCH}.${WHEEL_PATCH}"
+
+# Doing cmake work for the moment
+sed 's/@PYAGRUM_VERSION@/'${PYAGRUM_VERSION}'/' \
+  < ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/cmake/setup.in.py \
+  > ${WHEELHOUSE_DIR}/pyAgrum/setup.py
+
+sed 's/@PYAGRUM_VERSION@/'${PYAGRUM_VERSION}'/' \
+  < ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/cmake/functions.in.py \
+  > ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/functions.py
+
+sed 's/@PYAGRUM_VERSION@/'${PYAGRUM_VERSION}'/' \
+  < ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/cmake/__init__.in.py \
+  > ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/__init__.py
+
+cp ${PYAGRUM_DIR}/wheelhouse/files/* ${WHEELHOUSE_DIR}/pyAgrum/
+
+# Removing unecessary files from the wheelhouse
+rm -rf ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/cmake
+rm -rf ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/generated-files*
+rm -rf ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/wheelhouse
+rm -rf ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/sphinx
+rm -rf ${WHEELHOUSE_DIR}/pyAgrum/pyAgrum/testunits
+
+# Compile and install aGrUM
+cd ${AGRUM_DIR}
+/opt/python/cp36-cp36m/bin/python act install release aGrUM --withoutSQL --no-fun
+cd -
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin
+do
+  "${PYBIN}/pip" wheel ${WHEELHOUSE_DIR}/pyAgrum -w ${WHEELHOUSE_DIR}
+done
+
+# Bundle external shared libraries into the wheels
+for whl in ${WHEELHOUSE_DIR}/*.whl
+do
+  auditwheel repair "$whl" -w ${WHEELHOUSE_DIR}
+done
+
+# Moves wheels in shared Volume if exists
+mkdir -p /aGrUM/wheels
+cp ${WHEELHOUSE_DIR}/*.whl /aGrUM/wheels
+cd /aGrUM/wheels
+ls -1 | grep -v "manylinux1" | xargs rm -f
+ls -1 | grep "numpy" | xargs rm -f
+cd -
