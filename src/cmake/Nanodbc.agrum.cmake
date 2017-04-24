a275a398f4ebf3cca372e5982312e7f2dd9b3b3a
Lionel Torti <lionel.torti@gmail.com>
2017-04-24 12:20:13 +0200

    [cmake] Fixed an issue for discovering 'sql.h' headers when using SQL with aGrUM.

--- src/cmake/Nanodbc.agrum.cmake
+++ src/cmake/Nanodbc.agrum.cmake
@@ -11,8 +11,8 @@ set(NANODBC_ENABLE_LIBCXX "Use libc++ if available." ON) # ON)
 
 
 if(USE_NANODBC)
-	INCLUDE (CheckIncludeFileCXX)
-	CHECK_INCLUDE_FILE_CXX("sql.h" HAVE_SQLHEADER)
+	#INCLUDE (CheckIncludeFileCXX)
+	CHECK_INCLUDE_FILE("sql.h" HAVE_SQLHEADER)
 	IF(NOT HAVE_SQLHEADER)
 		set(USE_NANODBC OFF)
 		message(STATUS "** aGrUM Notification: Can not find <sql.h>. Nanodbc support is OFF.")
