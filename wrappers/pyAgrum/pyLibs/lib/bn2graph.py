e23ec45ef64fb0963e3564b2e78e50d1946ade0f
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2019-09-09 08:10:39 +0200

    [pyAgrum] dynamic bound for vertical histogramms

--- wrappers/pyAgrum/pyLibs/lib/bn2graph.py
+++ wrappers/pyAgrum/pyLibs/lib/bn2graph.py
@@ -179,6 +179,43 @@ def _getTitleHisto(p):
   (mu, std) = _stats(p)
   return "{}\n$\mu={:.2f}$; $\sigma={:.2f}$".format(var.name(), mu, std)
 
+def __limits(p):
+  """return vals and labs to show in the histograme
+  
+  Arguments:
+      p {gum.Potential} -- the marginal to analyze
+  """
+  var = p.variable(0)
+  la=[var.label(int(i)) for i in np.arange(var.domainSize())]
+  v = p.tolist()
+  nzmin=None
+  nzmax=None
+  l=len(v)-1
+  for i in range(l+1):
+    if v[i]!=0:
+      if nzmin is None:
+        if i>0:
+          nzmin=i-1
+        else:
+          nzmin=-1
+    if v[l-i]!=0:
+      if nzmax is None:
+        if i>0:
+          nzmax=l-i+1
+        else:
+          nzmax=-1
+                
+  mi=0 if nzmin in [-1,None] else nzmin
+  ma=l if nzmax in [-1,None] else nzmax
+
+  res=range(mi,ma+1)
+  lres=la[mi:ma+1]
+  if nzmin not in [-1,None]:
+    lres[0]="..."
+  if nzmax not in [-1,None]:
+    lres[-1] = "..."
+  
+  return res,[v[i] for i in res],lres
 
 def _getProbaV(p, scale=1.0):
   """
@@ -188,16 +225,15 @@ def _getProbaV(p, scale=1.0):
   :return: a matplotlib bar (vertical histogram) for a Potential p.
 
   """
-  var = p.variable(0)
-  ra = np.arange(var.domainSize())
-
+  ra, v,lv = __limits(p)
+  
   fig = plt.figure()
-  fig.set_figwidth(scale * var.domainSize() / 4.0)
+  fig.set_figwidth(scale * len(v) / 4.0)
   fig.set_figheight(scale * 2)
 
   ax = fig.add_subplot(111)
 
-  bars = ax.bar(ra, p.tolist(), align='center')
+  bars = ax.bar(ra, v, align='center')
   ma = p.max()
   
   for bar in bars:
@@ -207,11 +243,9 @@ def _getProbaV(p, scale=1.0):
       txt = txt_format.format(bar.get_height())
       ax.text( bar.get_x(),ma,txt, ha='left', va='top', rotation='vertical')
   
-  labels=[var.label(int(i)) for i in np.arange(var.domainSize())]
-
   ax.set_ylim(bottom=0, top=p.max())
   ax.set_xticks(ra)
-  ax.set_xticklabels(labels, rotation='vertical')
+  ax.set_xticklabels(lv, rotation='vertical')
   ax.set_title(_getTitleHisto(p))
   ax.get_yaxis().grid(True)
   ax.margins(0)
@@ -229,6 +263,7 @@ def _getProbaH(p, scale=1.0):
   """
   var = p.variable(0)
   ra = np.arange(var.domainSize())
+  
   ra_reverse = np.arange(var.domainSize() - 1, -1, -1)  # reverse order
   vx = ["{0}".format(var.label(int(i))) for i in ra_reverse]
 
