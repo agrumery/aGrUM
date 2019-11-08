4a0e3d74639a02bb1fe01fda9880dc4343736fee
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2019-11-08 15:54:03 +0100

    [pyAgrum] more robust ImportTestSuite (if some required packages are absent)

--- wrappers/pyAgrum/testunits/tests/ImportTestSuite.py
+++ wrappers/pyAgrum/testunits/tests/ImportTestSuite.py
@@ -1,6 +1,19 @@
 # -*- encoding: UTF-8 -*-
-
 import unittest
+
+try:
+  import matplotlib
+  matplotlibFound=True
+except ImportError:
+  matplotlibFound=False
+
+try:
+  import IPython
+  ipythonFound=True
+except ImportError:
+  ipythonFound=False
+
+
 
 from pyAgrumTestSuite import pyAgrumTestCase, addTests
 
@@ -9,16 +22,21 @@ class TestImport(pyAgrumTestCase):
   def testImport(self):
     try:
       import pyAgrum
-      import pyAgrum.lib.bn2graph
-      import pyAgrum.lib.bn2roc
       import pyAgrum.lib.bn2scores
       import pyAgrum.lib.bn_vs_bn
-      import pyAgrum.lib.dynamicBN
-      import pyAgrum.lib.ipython
-      import pyAgrum.lib.notebook
+      
+      if matplotlibFound:
+        if ipythonFound:
+          import pyAgrum.lib.ipython
+          import pyAgrum.lib.notebook
+
+        import pyAgrum.lib.bn2graph
+        import pyAgrum.lib.bn2roc
+        import pyAgrum.lib.dynamicBN
+
     except Exception as e:
       print(e)
-      self.assertFalse("Import error : "+e.message)
+      self.assertFalse("Import error : "+e.what())
     pass
       
 ts = unittest.TestSuite()
