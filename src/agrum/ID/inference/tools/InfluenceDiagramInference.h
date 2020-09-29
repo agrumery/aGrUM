fc81455a5522fad698d0276408a86a6d82ad9695
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2020-09-29 23:27:20 +0200

    [aGrUM-WIP] new ShaferShenoy inference for Influence Diagram (2)

---
+++ src/agrum/ID/inference/tools/InfluenceDiagramInference.h
@@ -0,0 +1,81 @@
+
+/**
+ *
+ *  Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) & Christophe GONZALES(@AMU)
+ *   info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/**
+ * @file
+ * @brief This file contains abstract class definitions influence diagrams
+ *        inference classes.
+ *
+ * @author Jean-Christophe MAGNAN and Pierre-Henri WUILLEMIN(@LIP6)
+ */
+
+#ifndef GUM_INFLUENCE_DIAGRAM_INFERENCE_H
+#define GUM_INFLUENCE_DIAGRAM_INFERENCE_H
+
+#include <agrum/ID/influenceDiagram.h>
+#include <agrum/tools/graphicalModels/inference/graphicalModelInference.h>
+
+namespace gum {
+  /**
+   * @class InfluenceDiagramInference InfluenceDiagramInference.h
+   *<agrum/ID/inference/InfluenceDiagramInference.h>
+   * @brief Abstract base class for inference engines in influence diagrams.
+   * @ingroup id_group
+   *
+   */
+  template < typename GUM_SCALAR >
+  class InfluenceDiagramInference: public GraphicalModelInference<GUM_SCALAR> {
+    public:
+    /**
+     * Default constructor
+     */
+    explicit InfluenceDiagramInference(
+       const InfluenceDiagram< GUM_SCALAR >* infDiag);
+
+    /**
+     * Destructor.
+     */
+    virtual ~InfluenceDiagramInference();
+
+    /// assigns a new ID to the inference engine
+    /** Assigns a new ID to the ShaferShenoyIDInference engine and sends messages
+     * to the descendants of ShaferShenoyIDInference to inform them that the ID has
+     * changed.
+     * @warning note that, by aGrUM's rule, the influence Diagram is not copied
+     * into the inference engine but only referenced. */
+    virtual void setID(const InfluenceDiagram< GUM_SCALAR >* infdiag);
+
+    /// Returns a constant reference over the IBayesNet referenced by this class
+    /** @throws UndefinedElement is raised if no Bayes net has been assigned to
+     * the inference. */
+    virtual const InfluenceDiagram< GUM_SCALAR >& ID() const final;
+
+    private:
+    /// assigns a influence diagram during the inference engine construction
+    void setIDDuringConstruction__(const InfluenceDiagram< GUM_SCALAR >* infdiag);
+  };
+
+} /* namespace gum */
+
+#include <agrum/ID/inference/tools/InfluenceDiagramInference_tpl.h>
+
+#endif /* GUM_INFLUENCE_DIAGRAM_INFERENCE_H */
---
+++ src/agrum/ID/inference/tools/InfluenceDiagramInference_tpl.h
@@ -0,0 +1,72 @@
+
+/**
+ *
+ *  Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) & Christophe GONZALES(@AMU)
+ *   info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/**
+ * @file
+ * @brief Implementation of the non pure virtual methods of
+ * InfluenceDiagramInference.
+ */
+
+// to ease parsers in IDE
+#include <agrum/tools/graphicalModels/inference/graphicalModelInference.h>
+#include <agrum/ID/inference/tools/InfluenceDiagramInference.h>
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+namespace gum {
+
+  // Default Constructor
+  template < typename GUM_SCALAR >
+  InfluenceDiagramInference< GUM_SCALAR >::InfluenceDiagramInference(
+     const InfluenceDiagram< GUM_SCALAR >* infDiag) :
+      GraphicalModelInference<GUM_SCALAR>(infDiag) {
+    GUM_CONSTRUCTOR(InfluenceDiagramInference);
+  }
+
+  // Destructor
+  template < typename GUM_SCALAR >
+  InfluenceDiagramInference< GUM_SCALAR >::~InfluenceDiagramInference() {
+    GUM_DESTRUCTOR(InfluenceDiagramInference);
+  }
+
+  template < typename GUM_SCALAR >
+  const InfluenceDiagram< GUM_SCALAR >&
+     InfluenceDiagramInference< GUM_SCALAR >::ID() const {
+    return static_cast< const InfluenceDiagram< GUM_SCALAR >& >(this->model);
+  }
+
+  // assigns a new BN to the inference engine
+  template < typename GUM_SCALAR >
+  void InfluenceDiagramInference< GUM_SCALAR >::setID(const InfluenceDiagram<GUM_SCALAR >* infdiag) {
+    this->setModel_(infdiag);
+  }
+
+  // assigns a BN to a newly constructed inference engine
+  template < typename GUM_SCALAR >
+  void InfluenceDiagramInference< GUM_SCALAR >::setIDDuringConstruction__(
+     const InfluenceDiagram< GUM_SCALAR >* infdiag) {
+    this->setModelDuringConstruction_(infdiag);
+  }
+
+} /* namespace gum */
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
