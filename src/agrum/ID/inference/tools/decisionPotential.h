b02c0c1147323a7a49f174e33fadb79b4765f3dd
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2020-11-19 13:40:00 +0100

    [aGrUM] new class for DecisionPotential

---
+++ src/agrum/ID/inference/tools/decisionPotential.h
@@ -0,0 +1,141 @@
+/**
+ *
+ *   Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) & Christophe GONZALES(@AMU)
+ *   info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/**
+ * @file
+ * @brief This file contains abstract class definitions influence diagrams
+ *        inference classes.
+ *
+ * @author Pierre-Henri WUILLEMIN(@LIP6) and Christophe Gonzalez(@
+ */
+#ifndef AGRUM_DECISIONPOTENTIAL_H
+#define AGRUM_DECISIONPOTENTIAL_H
+
+#include <agrum/tools/core/set.h>
+#include <agrum/tools/variables/discreteVariable.h>
+#include <agrum/tools/multidim/instantiation.h>
+#include <agrum/tools/multidim/potential.h>
+
+namespace gum {
+  /**
+   * @class DecisionPotential decisionPotential.h
+   *<agrum/ID/inference/decisionPotential.h>
+   * @brief Potential for optimization in LIMIDS (such as Single Policy Update)
+   */
+  template < typename GUM_SCALAR >
+  class DecisionPotential {
+    public:
+    Potential< GUM_SCALAR > probPot;
+    Potential< GUM_SCALAR > utilPot;
+
+    explicit DecisionPotential() {
+      GUM_CONSTRUCTOR(DecisionPotential);
+      probPot.fillWith(GUM_SCALAR(1));
+      utilPot.fillWith(GUM_SCALAR(0));
+    }
+
+    DecisionPotential(const Potential< GUM_SCALAR >& prob,
+                      const Potential< GUM_SCALAR >& util) :
+        probPot(prob),
+        utilPot(util) {
+      GUM_CONSTRUCTOR(DecisionPotential);
+    }
+
+    DecisionPotential(const DecisionPotential< GUM_SCALAR >& dp) :
+        probPot(dp.probPot, dp.utilPot) {
+      GUM_CONS_CPY(DecisionPotential);
+    }
+
+    DecisionPotential< GUM_SCALAR >&
+       operator=(const DecisionPotential< GUM_SCALAR >& src) {
+      GUM_OP_CPY(DecisionPotential);
+      if (&src == this) return *this;
+      probPot = src.probPot;
+      utilPot = src.utilPot;
+      return *this;
+    }
+
+    DecisionPotential(const DecisionPotential< GUM_SCALAR >&& dp) :
+        probPot(std::forward< Potential< GUM_SCALAR > >(dp.probPot),
+                std::forward< Potential< GUM_SCALAR > >(dp.utilPot)) {
+      GUM_CONS_MOV(DecisionPotential);
+    }
+
+    bool operator==(const DecisionPotential< GUM_SCALAR >& p) {
+      return ((p.probPot == this->probPot) && (p.utilPot == this->utilPot));
+    }
+
+    DecisionPotential< GUM_SCALAR >&
+       operator=(const DecisionPotential< GUM_SCALAR >&& src) {
+      GUM_OP_MOV(DecisionPotential);
+      if (&src == this) return *this;
+      probPot = std::forward< Potential< GUM_SCALAR > >(src.probPot);
+      utilPot = std::forward< Potential< GUM_SCALAR > >(src.utilPot);
+      return *this;
+    }
+
+
+    DecisionPotential< GUM_SCALAR >
+       operator*(const DecisionPotential< GUM_SCALAR >& dp1) const {
+      return DecisionPotential< GUM_SCALAR >::combination(*this, dp1);
+    }
+
+    DecisionPotential< GUM_SCALAR >
+       operator*=(const DecisionPotential< GUM_SCALAR >& dp1) {
+      *this = DecisionPotential< GUM_SCALAR >::combination(*this, dp1);
+      return *this;
+    }
+
+    DecisionPotential< GUM_SCALAR >
+       operator^(const Set< DiscreteVariable* >& onto) const {
+      return DecisionPotential< GUM_SCALAR >::marginalization(*this, onto);
+    }
+
+
+    static Potential< GUM_SCALAR >
+       divideEvenZero(const Potential< GUM_SCALAR >& p1,
+                      const Potential< GUM_SCALAR >& p2) {
+      Potential< GUM_SCALAR > res(p1);
+      Instantiation           I(res);
+      for (I.setFirst(); !I.end(); I.inc()) {
+        if (res[I] != 0) res[I] /= p2[I];
+      }
+      return res;
+    }
+
+    static DecisionPotential< GUM_SCALAR >
+       combination(const DecisionPotential< GUM_SCALAR >& dp1,
+                   const DecisionPotential< GUM_SCALAR >& dp2) {
+      return DecisionPotential< GUM_SCALAR >(dp1.probPot * dp2.probPot,
+                                             dp1.utilPot + dp2.utilPot);
+    }
+
+    static DecisionPotential< GUM_SCALAR >
+       marginalization(const DecisionPotential< GUM_SCALAR >& dp,
+                       const Set< DiscreteVariable* >&        onto) {
+      const auto pr = dp.probPot.margSumIn(onto);
+      return DecisionPotential(
+         pr, divideEvenZero((pr.probPot * pr.utilPot).margSumIn(onto), pr));
+    }
+  };
+}   // namespace gum
+
+#endif   // AGRUM_DECISIONPOTENTIAL_H
