482617ce6f2e203a18b1ac3c7d3d16d486d94fa1
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2020-12-30 01:16:52 +0100

    [pyAgrum] remove old deprecated class/method (since pyAgrum 0.12.0)

--- wrappers/pyAgrum/cmake/deprecated.in.py
+++ wrappers/pyAgrum/cmake/deprecated.in.py
@@ -25,14 +25,6 @@ def ShaferShenoyIDInference(infdiag):
   print("** A pyAgrum.ShaferShenoyLIMIDInference has been created.")
   return ShaferShenoyLIMIDInference(infdiag)
 
-def GibbsInference(bn):
-  """
-  Deprecated class. Use pyAgrum.GibbsSampling instead.
-  """
-  print("** pyAgrum.GibbsInference is deprecated in pyAgrum>0.12.0.")
-  print("** A pyAgrum.GibbsSampling has been created.")
-  return GibbsSampling(bn)
-
 def JTGenerator():
   """
   Deprecated class. Use pyAgrum.JunctionTreeGenerator instead.
@@ -58,36 +50,6 @@ def BruteForceKL(p,q):
   return ExactBNdistance(p,q)
 
 def _addDeprecatedMethods():
-  def deprecated_populate(pot, v):
-    """
-    Deprecated method in pyAgrum>0.12.0. See fillWith instead.
-    """
-    print("** pyAgrum.Potential.populate() is deprecated in pyAgrum>0.12.0.")
-    print("** pyAgrum.Potential.fillWith() will be called instead.")
-    return pot.fillWith(v)
-  Potential.populate = deprecated_populate
-
-  def deprecated_fill(pot, v):
-    """
-    Deprecated method in pyAgrum>0.12.0. See fillWith instead.
-    """
-    print("** pyAgrum.Potential.fill() is deprecated in pyAgrum>0.12.0.")
-    print("** pyAgrum.Potential.fillWith() will be called instead.")
-    return pot.fillWith(v)
-  Potential.fill = deprecated_fill
-
-  def deprecated_ids(gr):
-    """
-    Deprecated method in pyAgrum>0.12.0. See nodes instead.
-    """
-    print("** pyAgrum.*.ids() is deprecated in pyAgrum>0.12.0.")
-    print("** pyAgrum.*.nodes() will be called instead.")
-    return gr.nodes()
-  DiGraph.ids=deprecated_ids # and subclasses
-  UndiGraph.ids=deprecated_ids # and subclasses
-  EssentialGraph.ids=deprecated_ids
-  BayesNet.ids = deprecated_ids
-  InfluenceDiagram.ids = deprecated_ids
 
   def deprecated_setAprioriWeight(learner,weight):
     """
@@ -97,9 +59,6 @@ def _addDeprecatedMethods():
     print("** Please use the weight argument in `useApriori...` methods instead.")
     return
   BNLearner.setAprioriWeight=deprecated_setAprioriWeight
-
-#from .pyAgrum import BayesNet, EssentialGraph, MarkovBlanket
-
 
 
 _addDeprecatedMethods()
--- wrappers/pyAgrum/doc/sphinx/lib.classifier.rst
+++
@@ -1,8 +0,0 @@
-Bayesian network as classifier
-===============================
-
-.. image:: _static/classifiers.png
-
-.. automodule:: pyAgrum.lib.classifier
-    :members:
-    :noindex:
