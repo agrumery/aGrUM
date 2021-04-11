53e6c67a0e94cd0b51efcb1afc6df013dd18a49c
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2021-04-11 21:35:04 +0200

    [aGrUM] Fix an infamous bug: monocycle in DAG (thanks to Guy, GabF and Joanne)

--- src/testunits/module_BASE/DAGTestSuite.h
+++ src/testunits/module_BASE/DAGTestSuite.h
@@ -17,28 +17,6 @@
  *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
-
-/***************************************************************************
- *   (C) 2007-2013 by Christophe GONZALES(@AMU) and Pierre-Henri WUILLEMIN(@LIP6) *
- *   info_at_agrum_dot_org                                                  *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it wil be useful,        *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
-
 #include <iostream>
 
 #include <gumtest/AgrumTestSuite.h>
@@ -391,6 +369,12 @@ namespace gum_tests {
       gum::DAG graph = buildGraph();
       TS_ASSERT_EQUALS(graph.family(0), gum::NodeSet({0}))
       TS_ASSERT_EQUALS(graph.family(4), gum::NodeSet({1, 2, 3, 4}));
+    }
+
+    void testMonoCycle() {
+      gum::DAG graph;
+      auto x=graph.addNode();
+      TS_ASSERT_THROWS(graph.addArc(x,x),gum::Exception);
     }
   };
 }   // namespace gum_tests
