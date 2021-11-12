d0eab28f33f49ee33c9aba8f5f404473fa87074e
Christophe Gonzales <Christophe.Gonzales@lis-lab.fr>
2021-11-12 17:05:16 +0100

    adding new schedule storage operation

---
+++ src/agrum/tools/graphicalModels/inference/scheduler/scheduleStorage.h
@@ -0,0 +1,333 @@
+/**
+ *
+ *  Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) et Christophe GONZALES(@AMU)
+ *  info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/** @file
+ * @brief Class for storing multidimensional tables in scheduling inferences
+ *
+ * @author Christophe GONZALES(@AMU) and Pierre-Henri WUILLEMIN(@LIP6)
+ */
+
+#ifndef GUM_SCHEDULE_STORAGE_H
+#define GUM_SCHEDULE_STORAGE_H
+
+#include <type_traits>
+#include <string>
+#include <vector>
+
+#include <agrum/agrum.h>
+
+#include <agrum/tools/core/exceptions.h>
+#include <agrum/tools/core/set.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleMultiDim.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleOperation.h>
+
+namespace gum {
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+  namespace ScheduleStorageMethod {
+
+    /// A general structure to contain the method actually used to store
+    /// the multidimensional table into a data structure (container)
+    /** @warning we assume that the container will take the ownership of the
+     * table after the table has been inserted into it. Namely, if the
+     * container contains pointers to tables, then, upon destruction, it will
+     * take care of deallocating the pointers it contains. If the container
+     * directly contains tables, then those are supposed to be inserted into
+     * the container by moves. */
+    template < typename TABLE,
+               typename CONTAINER_TABLE,
+               template < typename, typename... > class CONTAINER,
+               template < typename > class ALLOC,
+               typename... CONTAINER_PARAMS >
+    struct Execution {
+      void execute(TABLE& table,
+                   CONTAINER< CONTAINER_TABLE, CONTAINER_PARAMS... >& container);
+    };
+
+    /// Specialized structure to store a Table into a Set<Table*>
+    /** @warning we assume that the container will take the ownership of the
+     * tables inserted into it, i.e., upon destruction, it will take care of
+     * deallocating the pointers it contains. */
+    template < typename TABLE,
+               template < typename > class ALLOC,
+               typename... CONTAINER_PARAMS >
+    struct Execution< TABLE, TABLE*, Set, ALLOC, CONTAINER_PARAMS... > {
+      void execute(TABLE& table,
+                   Set< TABLE*, CONTAINER_PARAMS... >& container);
+    };
+
+    /// a Specialized structure to store a Table into a vector<Table>
+    /** @warning we assume that the container will take the ownership of the
+     * tables inserted into it, i.e., the tables are supposed to be inserted
+     * into the container by moves. */
+     template < typename TABLE,
+               template < typename > class ALLOC,
+               typename... CONTAINER_PARAMS >
+    struct Execution< TABLE, TABLE, std::vector, ALLOC, CONTAINER_PARAMS... > {
+      void execute(TABLE& table,
+                   std::vector< TABLE, CONTAINER_PARAMS... >& container);
+    };
+
+  }
+
+#endif /* DOXYGEN_SHOULD_SKIP_THIS */
+
+  /**
+   * @class ScheduleStorage
+   * @brief Class for storing multidimensional tables into containers (sets, etc.)
+   * @headerfile scheduleStore.h <agrum/tools/graphicalModels/inference/scheduler/scheduleStore.h>
+   * @ingroup inference_schedule
+   */
+  template < typename TABLE,
+             template < typename, typename... > class CONTAINER,
+             template < typename > class ALLOC = std::allocator,
+             typename... CONTAINER_PARAMS >
+  class ScheduleStorage:
+      public ScheduleOperation< ALLOC >,
+      private ScheduleStorageMethod::Execution< typename std::remove_pointer<TABLE>::type,
+                                                TABLE, CONTAINER, ALLOC,
+                                                CONTAINER_PARAMS... > {
+    public:
+    using allocator_type = ALLOC< Idx >;
+    using SCHED_TABLE = typename std::remove_pointer<TABLE>::type;
+
+
+    // ############################################################################
+    /// @name Constructors / Destructors
+    // ############################################################################
+    /// @{
+
+    /// default constructor
+    /** @param table the ScheduleMultiDim containing the table that will
+     * eventually be stored.
+     * @warning table is stored only by reference within the ScheduleStorage.
+     */
+    explicit ScheduleStorage(const IScheduleMultiDim< ALLOC >& table,
+                             CONTAINER<TABLE, CONTAINER_PARAMS...>& container,
+                             const allocator_type& alloc = allocator_type());
+
+    /// copy constructor
+    ScheduleStorage(
+       const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >& from);
+
+    /// copy constructor with a given allocator
+    ScheduleStorage(
+       const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >& from,
+       const allocator_type&                                                  alloc);
+
+    /// move constructor
+    ScheduleStorage(
+       ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&& from);
+
+    /// move constructor with a given allocator
+    ScheduleStorage(
+       ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&& from,
+       const allocator_type&                                             alloc);
+
+    /// virtual copy constructor
+    ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >*
+       clone() const final;
+
+    /// virtual copy constructor with a given allocator
+    ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >*
+       clone(const allocator_type& alloc) const final;
+
+    /// destructor
+    virtual ~ScheduleStorage();
+
+    /// @}
+
+
+    // ############################################################################
+    /// @name Operators
+    // ############################################################################
+    /// @{
+
+    /// copy operator
+    ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&
+       operator=(
+          const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&);
+
+    /// move operator
+    ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&
+       operator=(
+       ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&&);
+
+    /// operator ==
+    /** Two ScheduleStorage are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments and the same containers to store their tables
+     * into. By Equal arguments, we stress that we mean that these
+     * ScheduleMultiDims have the same IDs*/
+    bool operator==(const ScheduleOperation< ALLOC >&) const final;
+
+    /// operator !=
+    /** Two ScheduleStorage are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments and the same containers to store their tables
+     * into. By Equal arguments, we stress that we mean that these
+     * ScheduleMultiDims have the same IDs*/
+    bool operator!=(const ScheduleOperation< ALLOC >&) const final;
+
+    /// operator ==
+    /** Two ScheduleStorage are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments and the same containers to store their tables
+     * into. By Equal arguments, we stress that we mean that these
+     * ScheduleMultiDims have the same IDs*/
+    virtual bool operator==(
+       const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&) const;
+
+    /// operator !=
+    /** Two ScheduleStorage are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments and the same containers to store their tables
+     * into. By Equal arguments, we stress that we mean that these
+     * ScheduleMultiDims have the same IDs*/
+    virtual bool operator!=(
+       const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&) const;
+
+    /// @}
+
+
+    // ############################################################################
+    /// @name Accessors/Modifiers
+    // ############################################################################
+    /// @{
+
+    /** @brief checks whether two ScheduleStorage have the same parameters
+     * (same variables and same content, and same container)
+     *
+     * ScheduleMultiDim Parameters having the same variables and the same content
+     * are essentially identical but they may have different Ids (so that they
+     * may not be ==).
+     */
+    bool hasSameArguments(const ScheduleOperation< ALLOC >&) const final;
+
+    /** @brief checks whether two ScheduleDeletion have the same parameters
+     * (same variables and same content, and same container)
+     *
+     * ScheduleMultiDim Parameters having the same variables and the same content
+     * are essentially identical but they may have different Ids (so that they
+     * may not be ==).
+     */
+    bool hasSameArguments(
+       const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&) const;
+
+    /** @brief checks whether two ScheduleProjection have similar parameters
+     * (same variables but not necessarily the same content)
+     */
+    bool hasSimilarArguments(const ScheduleOperation< ALLOC >&) const final;
+
+    /** @brief checks whether two ScheduleProjection have similar parameters
+     * (same variables but not necessarily the same content)
+     */
+    bool hasSimilarArguments(
+       const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&) const;
+
+    /// checks whether two ScheduleOperation perform the same operation
+    bool isSameOperation(const ScheduleOperation< ALLOC >&) const final;
+
+    /// checks whether two ScheduleOperation perform the same operation
+    bool isSameOperation(
+       const ScheduleStorage< TABLE, CONTAINER, ALLOC, CONTAINER_PARAMS... >&) const;
+
+    /// returns the argument of the storing function
+    const ScheduleMultiDim< SCHED_TABLE, ALLOC >& arg() const;
+
+    /// returns the sequence of arguments passed to the operation
+    const Sequence< const IScheduleMultiDim< ALLOC >* >& args() const final;
+
+    /// returns the sequence of ScheduleMultidim output by the operation
+    const Sequence< const IScheduleMultiDim< ALLOC >* >& results() const final;
+
+    /// really executes the operation
+    void execute() final;
+
+    /// indicates whether the operation has been executed
+    bool isExecuted() const final;
+
+    /// undo a previous execution, if any
+    /** throws OperationNotAllowed is raised if the operation is unable to
+     * perform undo */
+    void undo() final;
+
+    /// modifies the arguments of the operation
+    /** @throws SizeError is raised if the number of elements in new_args
+     * does not correspond to the number of arguments expected by the
+     * ScheduleOperation.
+     * @throws TypeError is raised if at least one element of new_args does
+     * not have a type compatible with what the ScheduleOperation expects.
+     */
+    void updateArgs(
+       const Sequence< const IScheduleMultiDim< ALLOC >* >& new_args) final;
+
+    /** @brief returns an estimation of the number of elementary operations
+     * needed to perform the ScheduleOperation */
+    double nbOperations() const final;
+
+    /// returns the memory consumption used during the operation
+    /** Actually, this function does not return a precise account of the memory
+     * used by the ScheduleOperation but a rough estimate based on the sizes
+     * of the tables involved in the operation.
+     * @return a pair of memory consumption: the first one is the maximum
+     * amount of memory used during the operation and the second one is the
+     * amount of memory still used at the end of the function ( the memory used
+     * by the resulting table )
+     * @warning This method does not checks whether the ScheduleMultiDim to be
+     * deleted is abstract or not: it assumes that it is not when the Store
+     * is performed. This enables to take into account the memory usage of a
+     * whole Schedule tree before performing any operation within it. */
+    std::pair< double, double > memoryUsage() const final;
+
+    /// displays the content of the operation
+    std::string toString() const final;
+
+    /// @}
+
+
+    private:
+    /// the table to store
+    ScheduleMultiDim< SCHED_TABLE, ALLOC >* _arg_;
+
+    /// the sequence of arguments passed to the operation
+    /** This method is convenient when using ScheduleOperation rather than
+     * directly using ScheduleBinaryCombination */
+    Sequence< const IScheduleMultiDim< ALLOC >* > _args_;
+
+    /// the sequence of ScheduleMultidim output by the operation
+    /** @warning Note that the Operation has always some output, even if
+     * it has not been executed. In this case, the outputs are abstract
+     * ScheduleMultiDim.
+     */
+    Sequence< const IScheduleMultiDim< ALLOC >* > _results_;
+
+    /// the container into which the table is stored
+    CONTAINER<TABLE, CONTAINER_PARAMS...>* _container_;
+
+    /// indicates whether the operation has been performed or not
+    bool _is_executed_{false};
+
+  };
+
+} /* namespace gum */
+
+// always include the template implementation
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleStorage_tpl.h>
+
+#endif /* GUM_SCHEDULE_STORAGE_H */
---
+++ src/testunits/module_BASE/ScheduleStorageTestSuite.h
@@ -0,0 +1,227 @@
+/**
+ *
+ *  Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) & Christophe GONZALES(@AMU)
+ *  info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include <sstream>
+
+#include <gumtest/AgrumTestSuite.h>
+#include <gumtest/testsuite_utils.h>
+
+#include <agrum/tools/core/set.h>
+#include <agrum/tools/core/utils_random.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleStorage.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleProjection.h>
+#include <agrum/tools/multidim/potential.h>
+#include <agrum/tools/variables/labelizedVariable.h>
+
+namespace gum_tests {
+
+  class ScheduleStorageTestSuite: public CxxTest::TestSuite {
+    public:
+    void test_construct() {
+      // reset the ids of the ScheduleMultiDim to avoid conflicts with other
+      // testunits
+      gum::IScheduleMultiDim<>::resetIdGenerator();
+
+      std::vector< gum::LabelizedVariable* > vars(10);
+
+      for (unsigned int i = 0; i < 10; ++i) {
+        std::stringstream str;
+        str << "x" << i;
+        std::string s = str.str();
+        vars[i] = new gum::LabelizedVariable(s, s, 2);
+      }
+
+      gum::Potential< double > pot1, pot2;
+      pot1 << *(vars[0]) << *(vars[2]) << *(vars[3]) << *(vars[4]);
+      pot2 << *(vars[0]) << *(vars[2]);
+      randomInit(pot1);
+      randomInit(pot2);
+      gum::ScheduleMultiDim< gum::Potential< double > > f1(pot1, true);
+      gum::ScheduleMultiDim< gum::Potential< double > > f1b(pot1, false);
+      gum::ScheduleMultiDim< gum::Potential< double > > f2(pot2, true);
+      gum::ScheduleMultiDim< gum::Potential< double > > f2b(pot2, false);
+
+      std::vector< gum::Potential< double > > v1;
+      gum::ScheduleStorage< gum::Potential< double >, std::vector > store1(f1, v1);
+      gum::ScheduleStorage< gum::Potential< double >, std::vector > store1bis(f1b, v1);
+
+      gum::Set< gum::Potential< double >* > set2;
+      gum::ScheduleStorage< gum::Potential< double >*, gum::Set > store2(f2, set2);
+      gum::Set< gum::Potential< double >* > set2b;
+      gum::ScheduleStorage< gum::Potential< double >*, gum::Set > store2bis(f2, set2b);
+      gum::ScheduleStorage< gum::Potential< double >*, gum::Set > store2ter(f2b, set2b);
+
+
+      TS_ASSERT(store1.implyDeletion());
+      TS_ASSERT(store1bis.implyDeletion());
+      TS_ASSERT(store2.implyDeletion());
+      TS_ASSERT(store2bis.implyDeletion());
+
+      TS_ASSERT(store1 != store1bis);
+      TS_ASSERT(store1.isSameOperation(store1bis));
+      TS_ASSERT(!(store1 == store1bis));
+      TS_ASSERT(store1.hasSimilarArguments(store1bis));
+      TS_ASSERT(store1.hasSameArguments(store1bis));
+
+      TS_ASSERT(store2 != store2bis);
+      TS_ASSERT(store2.isSameOperation(store2bis));
+      TS_ASSERT(!(store2 == store2bis));
+      TS_ASSERT(store2.hasSimilarArguments(store2bis));
+      TS_ASSERT(!store2.hasSameArguments(store2bis));
+
+      const gum::ScheduleOperation<>& store1const = store1;
+      TS_ASSERT(store1const.implyDeletion());
+      TS_ASSERT(store1 == store1const);
+      TS_ASSERT(!(store1 != store1const));
+      TS_ASSERT(store1.isSameOperation(store1const));
+      TS_ASSERT(store1.hasSimilarArguments(store1const));
+      TS_ASSERT(store1.hasSameArguments(store1const));
+
+      const gum::Sequence< const gum::IScheduleMultiDim<>* >& res1 =
+         store1.results();
+      TS_ASSERT(res1.empty());
+      TS_ASSERT(store1const.implyDeletion());
+      const gum::Sequence< const gum::IScheduleMultiDim<>* >&
+         xres1 = store1const.results();
+      TS_ASSERT(xres1.empty());
+
+      const auto& arg1 = store1.arg();
+      TS_ASSERT(&arg1 == &f1);
+      TS_ASSERT(arg1 == f1);
+
+      const auto& args1 = store1.args();
+      TS_ASSERT(args1.size() == gum::Size(1));
+      TS_ASSERT(arg1 == *args1[0]);
+
+      store1.execute();
+      TS_ASSERT(f1.isAbstract());
+      TS_ASSERT(store1.arg().isAbstract());
+      TS_ASSERT(v1.size() == std::size_t(1));
+      TS_ASSERT(pot1 == v1[0]);
+      TS_ASSERT_THROWS(store1.undo(), gum::OperationNotAllowed);
+
+      store1bis.execute();
+      TS_ASSERT(f1b.isAbstract());
+      TS_ASSERT(v1.size() == std::size_t(2));
+      TS_ASSERT(v1[0] == v1[1]);
+
+      store2.execute();
+      TS_ASSERT(store2.isExecuted());
+      TS_ASSERT(f2.isAbstract());
+      TS_ASSERT(store2.arg().isAbstract());
+      TS_ASSERT(set2.size() == gum::Size(1));
+      TS_ASSERT(**(set2.begin()) == pot2);
+      TS_ASSERT_THROWS(store2bis.execute(), gum::NullElement);
+
+      store2ter.execute();
+      TS_ASSERT(store2ter.isExecuted());
+      TS_ASSERT(f2b.isAbstract());
+      TS_ASSERT(store2ter.arg().isAbstract());
+      TS_ASSERT(set2b.size() == gum::Size(1));
+      TS_ASSERT(**(set2.begin()) == **(set2b.begin()));
+
+      TS_ASSERT(store2.nbOperations() == 1.0);
+      const std::pair< double, double > xxx2 = store2.memoryUsage();
+      TS_ASSERT(xxx2.first == -4.0);
+      TS_ASSERT(xxx2.second == -4.0);
+
+      const std::pair< double, double > xxx1 = store1.memoryUsage();
+      TS_ASSERT(xxx1.first == -16.0);
+      TS_ASSERT(xxx1.second == -16.0);
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f1c(pot1, true);
+      gum::ScheduleStorage< gum::Potential< double >, std::vector > store4(f1c, v1);
+      gum::ScheduleStorage< gum::Potential< double >, std::vector > store4b(store4);
+      TS_ASSERT(store4b.implyDeletion());
+      TS_ASSERT(! store4b.arg().isAbstract());
+
+      gum::ScheduleStorage< gum::Potential< double >, std::vector > store5(std::move(store4));
+      TS_ASSERT(store5.implyDeletion());
+      TS_ASSERT(! store5.arg().isAbstract());
+      store5.execute();
+      TS_ASSERT(store5.arg().isAbstract());
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f3(pot2, true);
+      TS_ASSERT(store1.isExecuted());
+      store1.updateArgs({&f3});
+      TS_ASSERT(!store1.arg().isAbstract());
+      TS_ASSERT(! store1.isExecuted());
+      TS_ASSERT(store1.implyDeletion());
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f4(pot2, true);
+      store2.updateArgs({&f4});
+      TS_ASSERT(store2 != store1);
+
+      store2bis.updateArgs({&f4});
+      gum::ScheduleStorage< gum::Potential< double >*, gum::Set > store3bis = store2bis;
+      store3bis = store2ter;
+      TS_ASSERT(store3bis.implyDeletion());
+      TS_ASSERT(store3bis == store2ter);
+      TS_ASSERT(store3bis != store2bis);
+
+      gum::ScheduleStorage< gum::Potential< double >*, gum::Set > store6 = store2bis;
+      store3bis = std::move(store2bis);
+      TS_ASSERT(store6 == store3bis);
+
+      gum::ScheduleStorage< gum::Potential< double >*, gum::Set >* store7 = store6.clone();
+      TS_ASSERT(store7->implyDeletion());
+      TS_ASSERT(store6 == *store7);
+      delete store7;
+
+      std::stringstream s;
+      s << "store ( " << f1b.toString() << " )";
+      TS_ASSERT(s.str() == store1bis.toString());
+
+      gum::Set< const gum::DiscreteVariable* > del_vars1;
+      del_vars1 << vars[0] << vars[3];
+      gum::ScheduleProjection< gum::Potential< double > > myproj(
+         f1b, del_vars1, myProjectMax);
+      TS_ASSERT(!store1bis.isSameOperation(myproj));
+      TS_ASSERT(!store1bis.hasSimilarArguments(myproj));
+
+      for (const auto ptr: set2)
+        delete ptr;
+      for (const auto ptr: set2b)
+        delete ptr;
+
+      for (unsigned int i = 0; i < vars.size(); ++i)
+        delete vars[i];
+    }
+
+    private:
+    static gum::Potential< double >
+       myProjectMax(const gum::Potential< double >&                 pot,
+                    const gum::Set< const gum::DiscreteVariable* >& del_vars) {
+      return gum::Potential< double >(gum::projectMax(*(pot.content()), del_vars));
+    }
+
+    // ==========================================================================
+    /// initialize randomly a table
+    // ==========================================================================
+    void randomInit(gum::Potential< double >& t) {
+      gum::Instantiation i(t);
+
+      for (i.setFirst(); !i.end(); ++i)
+        t.set(i, rand() * 100000.0f / RAND_MAX);
+    }
+  };
+
+} /* namespace gum_tests */
