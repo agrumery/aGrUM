ac059a58d59a039da50efcde0850c0be03719569
Christophe Gonzales <Christophe.Gonzales@lis-lab.fr>
2021-11-12 17:01:47 +0100

    adding new ScheduleMultiDim

---
+++ src/agrum/tools/graphicalModels/inference/scheduler/scheduleMultiDim.h
@@ -0,0 +1,335 @@
+/**
+ *
+ *  Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) et Christophe GONZALES(@AMU)
+ *  info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/** @file
+ * @brief a Wrapper for multi-dimensional tables used for scheduling inferences
+ *
+ * @author Christophe GONZALES(@AMU) and Pierre-Henri WUILLEMIN(@LIP6)
+ */
+#ifndef GUM_SCHEDULE_MULTI_DIM_H
+#define GUM_SCHEDULE_MULTI_DIM_H
+
+#include <string>
+#include <memory>
+
+#include <agrum/agrum.h>
+#include <agrum/tools/core/sequence.h>
+#include <agrum/tools/variables/discreteVariable.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/IScheduleMultiDim.h>
+
+namespace gum {
+
+  /**
+   * @class ScheduleMultiDim
+   * @brief a Wrapper for multi-dimensional tables used for scheduling inferences
+   * @headerfile scheduleMultiDim.h <agrum/tools/graphicalModels/inference/scheduler/scheduleMultiDim.h>
+   * @ingroup inference_schedule
+   *
+   * A ScheduleMultiDim is a wrapper that contains either a "real" multidmensional
+   * table or a nullptr if this table has not been computed yet (by an executor).
+   *
+   * Here is a brief piece of code that should highlight the concept:
+   * @code
+   * // some arbitrary potentials (to be initialized before going on)
+   * gum::Potential< float > pot1, pot2, pot3;
+   *
+   * // we wish to schedule ( pot1 + pot2 ) + pot3
+   * // so, first, create ScheduleMultiDims for wrapping these potentials
+   * gum::ScheduleMultiDim< gum::Potential< float > > f1 ( pot1 );
+   * gum::ScheduleMultiDim< gum::Potential< float > > f2 ( pot2 );
+   * gum::ScheduleMultiDim< gum::Potential< float > > f3 ( pot3 );
+   *
+   * // now schedule a combination (+) between f1 and f2
+   * gum::ScheduleCombine< gum::Potential< float >,
+   *                       gum::Potential< float >,
+   *                       gum::Potential< float > > comb1 ( &f1, &f2, add );
+   *
+   * // get the result and schedule it with f3
+   * const ScheduleMultiDim< gum::Potential< float > >& result1 = comb1.result ();
+   * gum::ScheduleCombine< gum::Potential< float >,
+   *                       gum::Potential< float >,
+   *                       gum::Potential< float > > comb2 ( &result2, &f3, add );
+   *
+   * // get the resulting ScheduleMultiDim
+   * const ScheduleMultiDim<gum::Potential< float > >& result2 = comb2.result ();
+   *
+   * // here, no addition has been performed yet. We just have a structure
+   * // that indicates which operations we wish to do. So, for the moment,
+   * // result1 and result2 do not contain real Potentials.
+   * // As such, they are called abstract and trying to get their "real"
+   * // Potential (using method multiDim()) would throw a NotFound exception.
+   * std::cout << result1.isAbstract ();
+   * std::cout << result2.isAbstract ();
+   * std::cout << ! f1.isAbstract ();
+   *
+   * // now, we can actually perform the operations
+   * comb1.execute ();
+   * std::cout << ! result1.isAbstract ();
+   * comb2.execute ();
+   *
+   * // here, we can display the content of the real Potential stored
+   * // into result2
+   * std::cout << result2.multiDim ();
+   * @endcode
+   *
+   * So, to summarize the key idea underlying Schedule* classes: these classes
+   * encapsulate operations to perform and multidim tables that should be passed
+   * as argument to these operations. But nothing is actually computed until
+   * the execute() methods of the scheduled operations are executed.
+   */
+  template < typename TABLE, template < typename > class ALLOC = std::allocator >
+  class ScheduleMultiDim: public IScheduleMultiDim< ALLOC > {
+    public:
+    using allocator_type = ALLOC< Idx >;
+
+    // ############################################################################
+    /// @name Constructors / Destructors
+    // ############################################################################
+    /// @{
+
+    /// constructs a ScheduleMultiDim by copying/referencing table
+    /** @param table the multidimensional table that we wish to be contained
+     * into the ScheduleMultiDim
+     * @param copy indicates whether the ScheduleMultiDim should use internally
+     * a copy of table or rather if it should just use a reference on an
+     * already created table.
+     * @param id if specified and different from 0, this will be the id of the
+     * constructed ScheduleMultiDim, else an Id is automatically provided to the
+     * ScheduleMultiDim.
+     * @param alloc the allocator used to allocated the tables within the
+     * ScheduleMultiDim.
+     */
+    explicit ScheduleMultiDim(const TABLE&          table,
+                              const bool            copy,
+                              const Idx             id = 0,
+                              const allocator_type& alloc = allocator_type());
+
+    /// constructs a ScheduleMultiDim by moving a table inside it
+    /** @param table the multidimensional table that we wish to be contained
+     * into the ScheduleMultiDim
+     * @param id if specified and different from 0, this will be the id of the
+     * constructed ScheduleMultiDim, else an Id is automatically provided to the
+     * ScheduleMultiDim.
+     * @param alloc the allocator used to allocated the tables within the
+     * ScheduleMultiDim.
+     */
+    explicit ScheduleMultiDim(TABLE&&               table,
+                              const Idx             id = 0,
+                              const allocator_type& alloc = allocator_type());
+
+    /// construct a ScheduleMultiDim for a multidimensional table yet to be built
+    /** The ScheduleMultiDim created is abstract, i.e., it does not contain a
+     * proper multidimensional table yet. However, the variables of the latter
+     * need be known to optimize inference processes
+     * @param id if specified and different from 0, this will be the id of the
+     * constructed ScheduleMultiDim, else an Id is automatically provided to the
+     * ScheduleMultiDim.
+     * @param alloc the allocator used to allocated the tables within the
+     * ScheduleMultiDim.
+     * @warning the sequence of variables is copied into the MultiDim. */
+    explicit ScheduleMultiDim(const Sequence< const DiscreteVariable* >& vars,
+                              const Idx                                  id = 0,
+                              const allocator_type& alloc = allocator_type());
+
+    /// copy constructor
+    ScheduleMultiDim(const ScheduleMultiDim< TABLE, ALLOC >& from);
+
+    /// copy constructor with a given allocator
+    ScheduleMultiDim(const ScheduleMultiDim< TABLE, ALLOC >& from,
+                     const allocator_type&                   alloc);
+
+    /// move constructor
+    ScheduleMultiDim(ScheduleMultiDim< TABLE, ALLOC >&& from);
+
+    /// move constructor with a given allocator
+    ScheduleMultiDim(ScheduleMultiDim< TABLE, ALLOC >&& from,
+                     const allocator_type&              alloc);
+
+    /// virtual copy constructor
+    virtual ScheduleMultiDim< TABLE, ALLOC >* clone() const;
+
+    /// virtual copy constructor with a given allocator
+    virtual ScheduleMultiDim< TABLE, ALLOC >*
+       clone(const allocator_type& alloc) const;
+
+    /// destructor
+    virtual ~ScheduleMultiDim();
+
+    /// @}
+
+
+    // ############################################################################
+    /// @name Operators
+    // ############################################################################
+
+    /// @{
+
+    /// copy operator
+    ScheduleMultiDim< TABLE, ALLOC >&
+       operator=(const ScheduleMultiDim< TABLE, ALLOC >& from);
+
+    /// move operator
+    ScheduleMultiDim< TABLE, ALLOC >&
+       operator=(ScheduleMultiDim< TABLE, ALLOC >&& from);
+
+    /// checks whether two ScheduleMultiDim have exactly the same ID
+    /** Two ScheduleMultiDim having the same ID should imply that they also
+     * contain the same sequences of variables and the same tables. */
+    virtual bool operator==(const ScheduleMultiDim< TABLE, ALLOC >&) const;
+
+    /// checks whether two ScheduleMultiDim have the same ID and type
+    /** Two ScheduleMultiDim having the same ID should imply that they also
+     * contain the same sequences of variables and the same tables. */
+    virtual bool operator==(const IScheduleMultiDim< ALLOC >&) const;
+
+    /// checks whether two ScheduleMultiDim have different IDs
+    /** Two ScheduleMultiDim having different IDs should mean that they either
+     * contain the different sequences of variables and/or different tables. */
+    virtual bool operator!=(const ScheduleMultiDim< TABLE, ALLOC >&) const;
+
+    /// checks whether two ScheduleMultiDim have different IDs or types
+    /** Two ScheduleMultiDim having different IDs should mean that they either
+     * contain the different sequences of variables and/or different tables. */
+    virtual bool operator!=(const IScheduleMultiDim< ALLOC >&) const;
+
+    /// @}
+
+
+    // ############################################################################
+    /// @name Accessors/Modifiers
+    // ############################################################################
+    /// @{
+
+    /// checks whether two ScheduleMultiDim have the same variables and types
+    /** Method hasSameVariables tests whether two ScheduleMultiDim contain the same
+     * sequences of variables. It is weaker than operator== in the sense that
+     * we do not check for the ScheduleMultiDim's ID nor for the content of
+     * the table stored into the IScheduleMultiDim, if any.
+     * @param m the ScheduleMultiDim with which we test similarity. */
+    virtual bool hasSameVariables(const IScheduleMultiDim< ALLOC >& m) const;
+
+    /// checks whether two ScheduleMultiDim have the same variables
+    /** Method hasSameVariables tests whether two ScheduleMultiDim contain the same
+     * sequences of variables. It is weaker than operator== in the sense that
+     * we do not check for the ScheduleMultiDim's ID nor for the content of
+     * the table stored into the IScheduleMultiDim, if any.
+     * @param m the ScheduleMultiDim with which we test similarity. */
+    virtual bool hasSameVariables(const ScheduleMultiDim< TABLE, ALLOC >& m) const;
+
+    /// checks whether two ScheduleMultiDim contain precisely the same table
+    /** By "contain precisely the same table", we mean that the actual tables
+     * pointed to by the ScheduleMultiDim are the same (i.e., either they are
+     * both not yet allocated but have the same sequences of variables or they
+     * have the same content) */
+    virtual bool hasSameContent(const IScheduleMultiDim< ALLOC >&) const;
+
+     /// checks whether two ScheduleMultiDim contain precisely the same table
+    /** By "contain precisely the same table", we mean that the actual tables
+     * pointed to by the ScheduleMultiDim are the same (i.e., either they are
+     * both not yet allocated but have the same sequences of variables or they
+     * have the same content) */
+    virtual bool hasSameContent(const ScheduleMultiDim< TABLE, ALLOC >&) const;
+
+    /// returns whether the ScheduleMultiDim contains a real table or not
+    /** @returns true if the ScheduleMultiDim is abstract, i.e., it is does not
+     * actually contains an allocated multidimensional table. */
+    bool isAbstract() const final;
+
+    /// indicates whether the ScheduleMultiDim contains a table and possesses it
+    /** Non-abstract ScheduleMultiDim can either contain a multidimensional table
+     * or they can reference an external table. This method indicates whether we
+     * are in the first case (true) or the second case (false) described above. */
+    bool containsMultiDim() const final;
+
+    /// if the ScheduleMultiDim is not abstract, make it abstract again
+    /** When a ScheduleMultiDim contains a multidimensional table, making it
+     * abstract is equivalent to freeing the table from memory and saying that
+     * the ScheduleMultiDim contains a null pointer. If the ScheduleMultiDim
+     * contains only a reference on an external table, then this table
+     * is kept untouched, but the ScheduleMultiDim now points toward a null
+     * pointer. Of course, if the ScheduleMultiDim was already abstract, then
+     * nothing is executed.
+     * @warning Making a ScheduleMultiDim abstract has an impact neither on
+     * the set of variables of the ScheduleMultiDim nor on its domain size.
+     * This just affects the table contained into the ScheduleMultiDim. */
+    void makeAbstract() final;
+
+    /** @brief returns the contained multidimensional table, if any, and make
+     * the ScheduleMultiDim abstract
+     *
+     * @throws NullElement is raised if the ScheduleMultiDim is abstract
+     * @throws OperationNotAllowed is raised if the ScheduleMultiDim is not
+     * abstract but it only references a MultiDim, it does not contain it */
+    TABLE exportMultiDim();
+
+    /** @brief returns the table actually contained in the ScheduleMultiDim
+     *
+     * @throws NotFound exception is thrown if the table does not exist
+     * yet (because it has not been computed yet) */
+    const TABLE& multiDim() const;
+
+    /// returns the set of variables involved in the ScheduleMultiDim
+    const Sequence< const DiscreteVariable* >& variablesSequence() const final;
+
+    /// returns the domain size the table would have after its creation
+    Size domainSize() const final;
+
+    /// assigns a new table inside the wrapper
+    /** @param table the multidimensional table that we wish to be contained
+     * into the ScheduleMultiDim
+     * @param copy indicates whether the ScheduleMultiDim should use internally
+     * a copy of table or rather if it should just use a reference on table
+     */
+    void setMultiDim(const TABLE& table, const bool copy);
+
+    /// sets by move a new table inside the wrapper
+    void setMultiDim(TABLE&& pot);
+
+    /// displays the content of the ScheduleMultiDim
+    std::string toString() const final;
+
+    /// @}
+
+
+    private:
+    /// the multidimensional table stored into the ScheduleMultiDim
+    TABLE* _table_{nullptr};
+
+    /// indicates whether __table is contained or referenced
+    bool _table_contained_{false};
+
+    /// returns the sequence of variables of the table
+    Sequence< const DiscreteVariable* > _var_sequence_;
+
+    /// the domain size of the table
+    Size _domain_size_{Size(0)};
+
+
+    /// remove the table if it is contained in the ScheduleMultiDim
+    void _removeTable_();
+  };
+
+} /* namespace gum */
+
+// always include the template implementation
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleMultiDim_tpl.h>
+
+#endif /* GUM_SCHEDULE_MULTI_DIM_H */
---
+++ src/testunits/module_BASE/ScheduleMultiDimTestSuite.h
@@ -0,0 +1,262 @@
+/**
+ *
+ *   Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) & Christophe GONZALES(@AMU)
+ *   info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include <sstream>
+
+#include <gumtest/AgrumTestSuite.h>
+#include <gumtest/testsuite_utils.h>
+#include <ressources/include/countedAlloc.h>
+
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleMultiDim.h>
+#include <agrum/tools/multidim/potential.h>
+#include <agrum/tools/variables/labelizedVariable.h>
+
+namespace gum_tests {
+
+  class ScheduleMultiDimTestSuite: public CxxTest::TestSuite {
+    public:
+    void test_construct() {
+      // reset the ids of the ScheduleMultiDim to avoid conflicts with other
+      // testunits
+      gum::IScheduleMultiDim<>::resetIdGenerator();
+
+      std::vector< gum::LabelizedVariable* > vars(10);
+
+      for (unsigned int i = 0; i < 10; ++i) {
+        std::stringstream str;
+        str << "x" << i;
+        std::string s = str.str();
+        vars[i] = new gum::LabelizedVariable(s, s, 10);
+      }
+
+      gum::Sequence< const gum::DiscreteVariable* > seq;
+      seq << vars[0] << vars[2] << vars[4];
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f1(seq);
+      TS_ASSERT(f1.isAbstract());
+      TS_ASSERT_THROWS(f1.multiDim(), gum::NotFound);
+      TS_ASSERT(f1.id() == gum::Idx(1));
+
+      std::string       s1 = f1.toString();
+      std::stringstream s2;
+      s2 << "<id: " << f1.id() << ", table: -->";
+      TS_ASSERT(s2.str() == s1);
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f1bis(seq, 10);
+      TS_ASSERT_THROWS(f1bis.multiDim(), gum::NotFound);
+      TS_ASSERT(f1.variablesSequence() == f1bis.variablesSequence());
+      TS_ASSERT(f1bis.id() == gum::Idx(10));
+      TS_ASSERT(f1bis.domainSize() == gum::Size(1000));
+
+      gum::ScheduleMultiDim< gum::Potential< double >, DebugCountedAlloc > f1ter(seq, 10);
+      TS_ASSERT(f1.variablesSequence() == f1ter.variablesSequence());
+      TS_ASSERT(f1ter.id() == gum::Idx(10));
+      TS_ASSERT(f1ter.domainSize() == gum::Size(1000));
+
+      gum::ScheduleMultiDim< gum::Potential< double >, DebugCountedAlloc > f1quart(seq);
+      TS_ASSERT(f1.variablesSequence() == f1quart.variablesSequence());
+      TS_ASSERT(f1quart.id() == gum::Idx(11));
+      TS_ASSERT(f1quart.domainSize() == gum::Size(1000));
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f2(f1);
+      TS_ASSERT(f2.isAbstract());
+      TS_ASSERT(f2.containsMultiDim() == false);
+      TS_ASSERT_THROWS(f2.multiDim(), gum::NotFound);
+      TS_ASSERT(f1 == f2);
+      TS_ASSERT(!(f1 != f2));
+      TS_ASSERT(f1 == dynamic_cast< gum::IScheduleMultiDim<>& >(f2));
+      TS_ASSERT(!(f1 != dynamic_cast< gum::IScheduleMultiDim<>& >(f2)));
+      TS_ASSERT(f1.id() == f2.id());
+      TS_ASSERT(f1.variablesSequence() == f2.variablesSequence());
+      TS_ASSERT(f2.domainSize() == gum::Size(1000));
+
+      f2.makeAbstract();
+      TS_ASSERT(f2.isAbstract());
+      TS_ASSERT(f2.containsMultiDim() == false);
+      TS_ASSERT(f2.domainSize() == gum::Size(1000));
+      TS_ASSERT(f2.variablesSequence() == seq);
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f3(seq);
+      TS_ASSERT(f1 != f3);
+      TS_ASSERT(f1.hasSameVariables(f3));
+      TS_ASSERT(f1.hasSameContent(f3));
+      TS_ASSERT(f1.hasSameVariables(dynamic_cast< gum::IScheduleMultiDim<>& >(f3)));
+      TS_ASSERT(f1.hasSameContent(dynamic_cast< gum::IScheduleMultiDim<>& >(f3)));
+      TS_ASSERT(f1.variablesSequence() == f3.variablesSequence());
+      TS_ASSERT(f3.domainSize() == gum::Size(1000));
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f3b(std::move(f3));
+      TS_ASSERT(f1 != f3b);
+      TS_ASSERT(f1.hasSameVariables(f3b));
+      TS_ASSERT(f1.hasSameContent(f3b));
+      TS_ASSERT(f1.variablesSequence() == f3b.variablesSequence())
+
+      gum::Potential< double > pot;
+      pot << *(vars[0]) << *(vars[2]) << *(vars[4]);
+      const std::vector< double > cpt(1000, 10);
+      pot.fillWith(cpt);
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f4(pot, true, 5);
+      TS_ASSERT(!f4.isAbstract());
+      TS_ASSERT(f1 != f4);
+      TS_ASSERT(f1.hasSameVariables(f4));
+      TS_ASSERT(! f1.hasSameContent(f4));
+      const gum::Potential< double >& res_pot = f4.multiDim();
+      TS_ASSERT(pot == res_pot);
+      TS_ASSERT(f4.containsMultiDim());
+      TS_ASSERT(f4.domainSize() == gum::Size(1000));
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f4bis(pot, true);
+      TS_ASSERT(!f4bis.isAbstract());
+      TS_ASSERT(f4.hasSameVariables(f4bis));
+      TS_ASSERT(f4.hasSameContent(f4bis));
+      TS_ASSERT(f4 != f4bis);
+      TS_ASSERT(!(f4 == f4bis));
+      const gum::Potential< double >& res_pot_bis = f4bis.multiDim();
+      TS_ASSERT(res_pot == res_pot_bis);
+      TS_ASSERT(f4bis.id() == gum::Idx(13));
+      TS_ASSERT(f4bis.domainSize() == gum::Size(1000));
+
+      gum::ScheduleMultiDim< gum::Potential< double >, DebugCountedAlloc > f4ter(
+         pot, true, 2);
+      TS_ASSERT(!f4ter.isAbstract());
+      const gum::Potential< double >& res_pot_ter = f4ter.multiDim();
+      TS_ASSERT(res_pot == res_pot_ter);
+      TS_ASSERT(f4ter.id() == gum::Idx(2));
+      TS_ASSERT(f4ter.domainSize() == gum::Size(1000));
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f4b(pot, false);
+      TS_ASSERT(!f4b.isAbstract());
+      TS_ASSERT(f4.multiDim() == f4b.multiDim());
+      TS_ASSERT(f4 != f4b);
+      TS_ASSERT(f4.hasSameVariables(f4b));
+      TS_ASSERT(!f4b.containsMultiDim());
+      TS_ASSERT(f4b.id() == gum::Idx(14));
+      TS_ASSERT(f4b.domainSize() == gum::Size(1000));
+
+      gum::Potential< double >                                   xxpot = res_pot;
+      gum::ScheduleMultiDim< gum::Potential< double >, DebugCountedAlloc > f4bt(
+         xxpot, false, 5);
+      TS_ASSERT(!f4bt.isAbstract());
+      TS_ASSERT(f4bt.multiDim() == f4.multiDim());
+      TS_ASSERT(f4ter.hasSameVariables(f4bt));
+      TS_ASSERT(!f4bt.containsMultiDim());
+      TS_ASSERT(f4bt.id() == gum::Idx(5));
+      TS_ASSERT(f4bt.domainSize() == gum::Size(1000));
+
+      gum::Potential< double >                          potx = res_pot;
+      gum::ScheduleMultiDim< gum::Potential< double > > f4c(std::move(potx));
+      TS_ASSERT(!f4c.isAbstract());
+      TS_ASSERT(f4c.containsMultiDim());
+      TS_ASSERT(f4.multiDim() == f4c.multiDim());
+      TS_ASSERT(f4.hasSameVariables(f4c));
+      TS_ASSERT(f4c.id() == gum::Idx(15));
+      TS_ASSERT(f4c.domainSize() == gum::Size(1000));
+
+      gum::Potential< double >                          potxx = res_pot;
+      gum::ScheduleMultiDim< gum::Potential< double > > f4d(std::move(potxx), 10);
+      TS_ASSERT(!f4d.isAbstract());
+      TS_ASSERT(f4d.containsMultiDim());
+      TS_ASSERT(f4.multiDim() == f4d.multiDim());
+      TS_ASSERT(f4.hasSameVariables(f4d));
+      TS_ASSERT(f4 != f4d);
+      TS_ASSERT(f4d.id() == gum::Idx(10));
+
+      f4d.makeAbstract();
+      TS_ASSERT(f4d.isAbstract());
+      TS_ASSERT(f4d.containsMultiDim() == false);
+      TS_ASSERT(f4d.domainSize() == gum::Size(1000));
+      TS_ASSERT(f4d.variablesSequence() == seq);
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f5(f4);
+      TS_ASSERT(!f5.isAbstract());
+      TS_ASSERT(f5 == f4);
+      TS_ASSERT(f5.hasSameContent(f4));
+      TS_ASSERT(f4.id() == f5.id());
+      TS_ASSERT(f4.multiDim() == res_pot);
+
+      std::string       s5 = f5.toString();
+      std::stringstream s6;
+      s6 << "<id: 5, table: " << f5.multiDim().content() << ">";
+      TS_ASSERT(s6.str() == s5);
+
+      gum::Potential< double > pot2;
+      pot2 << *(vars[1]) << *(vars[2]) << *(vars[4]);
+      const std::vector< double > cpt2(1000, 42);
+      pot2.fillWith(cpt2);
+      f4.setMultiDim(pot2, true);
+      gum::ScheduleMultiDim< gum::Potential< double > > f6(f4);
+      TS_ASSERT(f6.multiDim() == pot2);
+      f6.setMultiDim(pot, true);
+      TS_ASSERT(f6.multiDim() == pot);
+      TS_ASSERT(f6.multiDim() != f4.multiDim());
+
+      TS_ASSERT(f4.containsMultiDim());
+      f6 = f4;
+      TS_ASSERT(f6 == f4);
+      TS_ASSERT(f6.hasSameVariables(f4));
+      TS_ASSERT(f6.hasSameContent(f4));
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f7(pot, false, 5);
+      f7 = f4;
+      TS_ASSERT(f7 == f4);
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f8(pot, false, 8);
+      gum::ScheduleMultiDim< gum::Potential< double > > f9(pot2, true, 9);
+      TS_ASSERT(f9.multiDim() != f8.multiDim());
+      f9 = f8;
+      TS_ASSERT(!f8.containsMultiDim());
+      TS_ASSERT(!f9.containsMultiDim());
+      TS_ASSERT(f8 == f9);
+      TS_ASSERT(f8.hasSameVariables(f9));
+      TS_ASSERT(f8.hasSameContent(f9));
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f10(pot, false, 10);
+      gum::ScheduleMultiDim< gum::Potential< double > > f11(pot2, true, 10);
+      f10 = std::move(f11);
+      TS_ASSERT(f10.containsMultiDim());
+      TS_ASSERT(f10.variablesSequence() == pot2.variablesSequence());
+
+      auto pot3 = f10.exportMultiDim();
+      TS_ASSERT(f10.isAbstract());
+      TS_ASSERT(!f10.containsMultiDim());
+      TS_ASSERT(pot3 == pot2);
+      f10.makeAbstract();
+      TS_ASSERT(f10.isAbstract());
+
+      const auto pot4 = pot2;
+      f10.setMultiDim(std::move(pot2));
+      TS_ASSERT(f10.containsMultiDim());
+      TS_ASSERT(f10.multiDim() == pot4);
+
+      gum::ScheduleMultiDim< gum::Potential< double > >* f12 = f10.clone();
+      TS_ASSERT(*f12 == f10);
+      TS_ASSERT(f10.hasSameVariables(*f12));
+      TS_ASSERT(f10.hasSameContent(*f12));
+
+      delete f12;
+
+      for (unsigned int i = 0; i < vars.size(); ++i)
+        delete vars[i];
+    }
+  };
+
+} /* namespace gum_tests */
