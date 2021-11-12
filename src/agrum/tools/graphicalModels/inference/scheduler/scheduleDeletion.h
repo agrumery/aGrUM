01396e79c50681f0658767a37f725adfc2801e2a
Christophe Gonzales <Christophe.Gonzales@lis-lab.fr>
2021-11-12 17:04:23 +0100

    adding new schedule deletion

---
+++ src/agrum/tools/graphicalModels/inference/scheduler/scheduleDeletion.h
@@ -0,0 +1,267 @@
+/**
+ *
+ *  Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) et Christophe GONZALES(@AMU)
+ *  info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/** @file
+ * @brief an operation to force a ScheduleMultiDim to be abstract
+ *
+ * @author Christophe GONZALES(@AMU) and Pierre-Henri WUILLEMIN(@LIP6)
+ */
+
+#ifndef GUM_SCHEDULE_DELETION_H
+#define GUM_SCHEDULE_DELETION_H
+
+#include <string>
+
+#include <agrum/agrum.h>
+
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleMultiDim.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleOperation.h>
+
+namespace gum {
+
+  /**
+   * @class ScheduleDeletion
+   * @brief an operation to make a ScheduleMultiDim abstract and free its memory
+   * @headerfile scheduleDeletion.h <agrum/tools/graphicalModels/inference/scheduler/scheduleDeletion.h>
+   *
+   * @ingroup inference_schedule
+   * The following code gives an example of the usage of ScheduleDeletion:
+   * @code
+   * // create the multidim
+   * Potential< float > p1 (....), p2(....);
+   * ScheduleMultiDim< Potential< float > > t1(p1, true); // contains a copy of pot
+   * ScheduleMultiDim< Potential< float > > t2(p2, false); // contains a ref on pot
+   *
+   * // create the ScheduleDeletions
+   * ScheduleDeletion< Potential<float> > del1(t1), del2(t2);
+   *
+   * // applying del1 consists of releasing the memory of the potential it
+   * // contains and, subsequently, of making the ScheduleMultiDim abstract
+   * del1.execute();
+   *
+   * // applying del2 consists of making ScheduleMultiDim t2 abstract. As t2
+   * // contained only a reference on p2, the latter remains unchanged.
+   * @encode
+   */
+  template < typename TABLE, template < typename > class ALLOC = std::allocator >
+  class ScheduleDeletion: public ScheduleOperation< ALLOC > {
+    public:
+    using allocator_type = ALLOC< Idx >;
+
+    // ############################################################################
+    /// @name Constructors / Destructors
+    // ############################################################################
+    /// @{
+
+    /// default constructor
+    /** @param table the ScheduleMultiDim containing the table that will
+     * be deleted.
+     * @warning table is stored only by reference within the ScheduleDeletion.
+     */
+    explicit ScheduleDeletion(const ScheduleMultiDim< TABLE, ALLOC >& table,
+                              const allocator_type& alloc = allocator_type());
+
+    /// copy constructor
+    ScheduleDeletion(const ScheduleDeletion< TABLE, ALLOC >& from);
+
+    /// copy constructor with a given allocator
+    ScheduleDeletion(const ScheduleDeletion< TABLE, ALLOC >& from,
+                     const allocator_type&                   alloc);
+
+    /// move constructor
+    ScheduleDeletion(ScheduleDeletion< TABLE, ALLOC >&& from);
+
+    /// move constructor with a given allocator
+    ScheduleDeletion(ScheduleDeletion< TABLE, ALLOC >&& from,
+                     const allocator_type&              alloc);
+
+    /// virtual copy constructor
+    ScheduleDeletion< TABLE, ALLOC >* clone() const final;
+
+    /// virtual copy constructor with a given allocator
+    ScheduleDeletion< TABLE, ALLOC >*
+       clone(const allocator_type& alloc) const final;
+
+    /// destructor
+    virtual ~ScheduleDeletion();
+
+    /// @}
+
+
+    // ############################################################################
+    /// @name Operators
+    // ############################################################################
+    /// @{
+
+    /// copy operator
+    ScheduleDeletion< TABLE, ALLOC >&
+       operator=(const ScheduleDeletion< TABLE, ALLOC >&);
+
+    /// move operator
+    ScheduleDeletion< TABLE, ALLOC >&
+       operator=(ScheduleDeletion< TABLE, ALLOC >&&);
+
+    /// operator ==
+    /** Two operations are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments. By Equal arguments, we stress that we mean
+     * that these ScheduleMultiDims have the same IDs*/
+    bool operator==(const ScheduleOperation< ALLOC >&m) const final;
+
+    /// operator !=
+    /** Two operations are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments. By Equal arguments, we stress that we mean
+     * that these ScheduleMultiDims have the same IDs*/
+    bool operator!=(const ScheduleOperation< ALLOC >&m) const final;
+
+    /// operator ==
+    /** Two operations are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments. By Equal arguments, we stress that we mean
+     * that these ScheduleMultiDims have the same IDs*/
+    virtual bool operator==(const ScheduleDeletion< TABLE, ALLOC >&) const;
+
+    /// operator !=
+    /** Two operations are identical if and only if they have equal (==)
+     * ScheduleMultiDim arguments. By Equal arguments, we stress that we mean
+     * that these ScheduleMultiDims have the same IDs*/
+    virtual bool operator!=(const ScheduleDeletion< TABLE, ALLOC >&) const;
+
+    /// @}
+
+
+    // ############################################################################
+    /// @name Accessors/Modifiers
+    // ############################################################################
+    /// @{
+
+    /** @brief checks whether two ScheduleDeletion have the same parameters
+     * (same variables and same content)
+     *
+     * ScheduleMultiDim Parameters having the same variables and the same content
+     * are essentially identical but they may have different Ids (so that they
+     * may not be ==).
+     */
+    bool hasSameArguments(const ScheduleOperation< ALLOC >&) const final;
+
+    /** @brief checks whether two ScheduleDeletion have the same parameters
+     * (same variables and same content)
+     *
+     * ScheduleMultiDim Parameters having the same variables and the same content
+     * are essentially identical but they may have different Ids (so that they
+     * may not be ==).
+     */
+    bool hasSameArguments(const ScheduleDeletion< TABLE, ALLOC >&) const;
+
+    /** @brief checks whether two ScheduleProjection have similar parameters
+     * (same variables but not necessarily the same content)
+     */
+    bool hasSimilarArguments(const ScheduleOperation< ALLOC >&) const final;
+
+    /** @brief checks whether two ScheduleProjection have similar parameters
+     * (same variables but not necessarily the same content)
+     */
+    bool hasSimilarArguments(const ScheduleDeletion< TABLE, ALLOC >&) const;
+
+    /// checks whether two ScheduleOperation perform the same operation
+    bool isSameOperation(const ScheduleOperation< ALLOC >&) const final;
+
+    /// checks whether two ScheduleOperation perform the same operation
+    bool isSameOperation(const ScheduleDeletion< TABLE, ALLOC >&) const;
+
+    /// returns the argument of the projection
+    const ScheduleMultiDim< TABLE, ALLOC >& arg() const;
+
+    /// returns the sequence of arguments passed to the operation
+    const Sequence< const IScheduleMultiDim< ALLOC >* >& args() const final;
+
+    /// returns the sequence of ScheduleMultidim output by the operation
+    const Sequence< const IScheduleMultiDim< ALLOC >* >& results() const final;
+
+    /// modifies the arguments of the operation
+    /** @throws SizeError is raised if the number of elements in new_args
+     * does not correspond to the number of arguments expected by the
+     * ScheduleOperation.
+     * @throws TypeError is raised if at least one element of new_args does
+     * not have a type compatible with what the ScheduleOperation expects.
+     */
+    void updateArgs(
+       const Sequence< const IScheduleMultiDim< ALLOC >* >& new_args) final;
+
+    /// indicates whether the operation has been executed
+    bool isExecuted() const final;
+
+    /// executes the operation
+    void execute() final;
+
+    /// undo a previous execution, if any
+    /** @throws OperationNotAllowed is raised if we wish to undo a deletion because
+     * no information is stored about the original table when this one
+     * is deleted. */
+    void undo() final;
+
+    /** @brief returns an estimation of the number of elementary operations
+     * needed to perform the ScheduleOperation */
+    double nbOperations() const final;
+
+    /// returns the memory consumption used during the operation
+    /** Actually, this function does not return a precise account of the memory
+     * used by the ScheduleOperation but a rough estimate based on the sizes
+     * of the tables involved in the operation.
+     * @return a pair of memory consumption: the first one is the maximum
+     * amount of memory used during the operation and the second one is the
+     * amount of memory still used at the end of the function ( the memory used
+     * by the resulting table )
+     * @warning This method does not checks whether the ScheduleMultiDim to be
+     * deleted is abstract or not: it assumes that it is not when the deletion
+     * is performed. This enables to take into account the memory usage of a
+     * whole Schedule tree before performing any operation within it. */
+    std::pair< double, double > memoryUsage() const final;
+
+    /// displays the content of the operation
+    std::string toString() const final;
+
+    /// @}
+
+    private:
+    /// the table to delete, i.e., to make abstract
+    ScheduleMultiDim< TABLE, ALLOC >* _arg_{nullptr};
+
+    /// the sequence of arguments passed to the operation
+    /** This method is convenient when using ScheduleOperation rather than
+     * directly using ScheduleBinaryCombination */
+    Sequence< const IScheduleMultiDim< ALLOC >* > _args_;
+
+    /// the sequence of ScheduleMultidim output by the operation
+    /** @warning Note that the Operation has always some output, even if
+     * it has not been executed. In this case, the outputs are abstract
+     * ScheduleMultiDim.
+     */
+    Sequence< const IScheduleMultiDim< ALLOC >* > _results_;
+
+    /// indicates whether the operation has been performed or not
+    bool _is_executed_{false};
+  };
+
+} /* namespace gum */
+
+// always include the template implementation
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleDeletion_tpl.h>
+
+#endif /* GUM_SCHEDULE_DELETION_H */
---
+++ src/testunits/module_BASE/ScheduleDeletionTestSuite.h
@@ -0,0 +1,191 @@
+/**
+ *
+ *  Copyright 2005-2020 Pierre-Henri WUILLEMIN(@LIP6) & Christophe GONZALES(@AMU)
+ *  info_at_agrum_dot_org
+ *
+ *  This library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include <sstream>
+
+#include <gumtest/AgrumTestSuite.h>
+#include <gumtest/testsuite_utils.h>
+
+#include <agrum/tools/core/utils_random.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleDeletion.h>
+#include <agrum/tools/graphicalModels/inference/scheduler/scheduleProjection.h>
+#include <agrum/tools/multidim/potential.h>
+#include <agrum/tools/variables/labelizedVariable.h>
+
+namespace gum_tests {
+
+  class ScheduleDeletionTestSuite: public CxxTest::TestSuite {
+   public:
+    void test_construct() {
+      // reset the ids of the ScheduleMultiDim to avoid conflicts with other
+      // testunits
+      gum::IScheduleMultiDim<>::resetIdGenerator();
+
+      std::vector< gum::LabelizedVariable* > vars(10);
+
+      for (unsigned int i = 0; i < 10; ++i) {
+        std::stringstream str;
+        str << "x" << i;
+        std::string s = str.str();
+        vars[i] = new gum::LabelizedVariable(s, s, 2);
+      }
+
+      gum::Potential< double > pot1, pot2;
+      pot1 << *(vars[0]) << *(vars[2]) << *(vars[3]) << *(vars[4]);
+      pot2 << *(vars[0]) << *(vars[2]);
+      randomInit(pot1);
+      randomInit(pot2);
+      gum::ScheduleMultiDim< gum::Potential< double > > f1(pot1, false);
+      gum::ScheduleMultiDim< gum::Potential< double > > f1b(pot1, true);
+      gum::ScheduleMultiDim< gum::Potential< double > > f2(pot2, true);
+
+      gum::ScheduleDeletion< gum::Potential< double > > del1(f1);
+      gum::ScheduleDeletion< gum::Potential< double > > del1b(f1b);
+      gum::ScheduleDeletion< gum::Potential< double > > del2(f2);
+
+      TS_ASSERT(del1.implyDeletion());
+      TS_ASSERT(del1b.implyDeletion());
+      TS_ASSERT(del2.implyDeletion());
+
+      TS_ASSERT(del1 != del1b);
+      TS_ASSERT(del1.isSameOperation(del1b));
+      TS_ASSERT(!(del1 == del1b));
+      TS_ASSERT(del1.hasSimilarArguments(del1b));
+      TS_ASSERT(del1.hasSameArguments(del1b));
+
+      const gum::ScheduleOperation<>& del1const = del1;
+      TS_ASSERT(del1const.implyDeletion());
+      TS_ASSERT(del1 == del1const);
+      TS_ASSERT(!(del1 != del1const));
+      TS_ASSERT(del1.isSameOperation(del1const));
+      TS_ASSERT(del1.hasSimilarArguments(del1const));
+     TS_ASSERT(del1.hasSameArguments(del1const));
+
+      const gum::Sequence< const gum::IScheduleMultiDim<>* >& res1 =
+        del1.results();
+      TS_ASSERT(res1.empty());
+      const gum::ScheduleDeletion< gum::Potential< double > > xdel1const (del1);
+      TS_ASSERT(xdel1const.implyDeletion());
+      const gum::Sequence< const gum::IScheduleMultiDim<>* >&
+         xres1 = xdel1const.results();
+      TS_ASSERT(xres1.empty());
+
+      const auto& arg1 = del1.arg();
+      TS_ASSERT(&arg1 == &f1);
+      TS_ASSERT(arg1 == f1);
+
+      const auto& args1 = del1.args();
+      TS_ASSERT(args1.size() == gum::Size(1));
+      TS_ASSERT(arg1 == *args1[0]);
+
+      del1b.execute();
+      TS_ASSERT(! f1.isAbstract());
+      TS_ASSERT(del1b.arg().isAbstract());
+      TS_ASSERT(del1b.implyDeletion());
+
+      del1.execute();
+      TS_ASSERT(del1.isExecuted());
+      TS_ASSERT(f1.isAbstract());
+      TS_ASSERT(arg1.isAbstract());
+
+      TS_ASSERT_THROWS(del1.undo(), gum::OperationNotAllowed);
+
+      TS_ASSERT(del2.nbOperations() == 1.0);
+      const std::pair< double, double > xxx2 = del2.memoryUsage();
+      TS_ASSERT(xxx2.first == -4.0);
+      TS_ASSERT(xxx2.second == -4.0);
+
+      const std::pair< double, double > xxx1 = del1.memoryUsage();
+      TS_ASSERT(xxx1.first == -16.0);
+      TS_ASSERT(xxx1.second == -16.0);
+
+      gum::ScheduleDeletion< gum::Potential< double > > del4(del2);
+      TS_ASSERT(del4.implyDeletion());
+      TS_ASSERT(! del4.arg().isAbstract());
+
+      gum::ScheduleDeletion< gum::Potential< double > > del5(std::move(del4));
+      TS_ASSERT(del5.implyDeletion());
+      TS_ASSERT(! del5.arg().isAbstract());
+      del5.execute();
+      TS_ASSERT(del5.arg().isAbstract());
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f3(pot2, true);
+      TS_ASSERT(del1.isExecuted());
+      del1.updateArgs({&f3});
+      TS_ASSERT(!del1.arg().isAbstract());
+      TS_ASSERT(! del1.isExecuted());
+      TS_ASSERT(del1.implyDeletion());
+
+      gum::ScheduleMultiDim< gum::Potential< double > > f4(pot2, true);
+      del2.updateArgs({&f4});
+      TS_ASSERT(del2 != del1);
+      TS_ASSERT(del1.hasSameArguments(del2));
+
+      del1 = del2;
+      TS_ASSERT(del1.implyDeletion());
+      TS_ASSERT(del2.implyDeletion());
+      TS_ASSERT(del2 == del1);
+      TS_ASSERT(del1 != del1b);
+      gum::ScheduleDeletion< gum::Potential< double > > del6 = del1b;
+      TS_ASSERT(del6.implyDeletion());
+      del1 = std::move(del6);
+      TS_ASSERT(del1 == del1b);
+
+      gum::ScheduleDeletion< gum::Potential< double > >* del7 = del1.clone();
+      TS_ASSERT(del7->implyDeletion());
+      TS_ASSERT(del1 == *del7);
+      TS_ASSERT(*del7 != del2);
+      delete del7;
+
+      std::stringstream s;
+      s << "delete ( " << f1b.toString() << " )";
+      TS_ASSERT(s.str() == del1.toString());
+
+      gum::Set< const gum::DiscreteVariable* > del_vars1;
+      del_vars1 << vars[0] << vars[3];
+      gum::ScheduleProjection< gum::Potential< double > > myproj(
+         f1b, del_vars1, myProjectMax);
+      TS_ASSERT(!del2.isSameOperation(myproj));
+      TS_ASSERT(!del2.hasSimilarArguments(myproj));
+
+      for (unsigned int i = 0; i < vars.size(); ++i)
+        delete vars[i];
+    }
+
+    private:
+    static gum::Potential< double >
+       myProjectMax(const gum::Potential< double >&                 pot,
+                    const gum::Set< const gum::DiscreteVariable* >& del_vars) {
+      return gum::Potential< double >(gum::projectMax(*(pot.content()), del_vars));
+    }
+
+     // ==========================================================================
+    /// initialize randomly a table
+    // ==========================================================================
+    void randomInit(gum::Potential< double >& t) {
+      gum::Instantiation i(t);
+
+      for (i.setFirst(); !i.end(); ++i)
+        t.set(i, rand() * 100000.0f / RAND_MAX);
+    }
+  };
+
+} /* namespace gum_tests */
