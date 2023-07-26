88750db3a6ad4d5ff83c2528ba1d34d358cbc097
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2023-07-26 11:23:36 +0200

    [pyAgrum] new config for graphviz layout and rankdir + change in notebook using this feature.

--- wrappers/pyAgrum/pyLibs/lib/notebook.py
+++ wrappers/pyAgrum/pyLibs/lib/notebook.py
@@ -266,6 +266,8 @@ def _reprGraph(gr, size, asString, format=None):
   """
   if size is not None:
     gr.set_size(size)
+  gr.set_rankdir(gum.config["notebook", "graph_rankdir"])
+  gr.set_layout(gum.config["notebook", "graph_layout"])
 
   if format is None:
     format = gum.config["notebook", "graph_format"]
