0c50d7e5e20162d32ee47e83e0744f009abfec9f
Pierre-Henri Wuillemin <pierre-henri.wuillemin@lip6.fr>
2023-10-19 13:45:22 +0200

    [pyAgrum] 3.12 in all metadatas.

--- wrappers/pyAgrum/cmake/setup.in.py
+++ wrappers/pyAgrum/cmake/setup.in.py
@@ -75,6 +75,7 @@ classifiers = [
   'Programming Language :: Python :: 3.9',
   'Programming Language :: Python :: 3.10',
   'Programming Language :: Python :: 3.11',
+  'Programming Language :: Python :: 3.12',
   'Topic :: Scientific/Engineering :: Artificial Intelligence',
   'Topic :: Scientific/Engineering :: Information Analysis',
   'Topic :: Scientific/Engineering :: Mathematics',
