c65a87492837ee314c55fd917a2a92fea0028b1b
Gaspard Ducamp <ducamp.gaspard@gmail.com>
2024-02-15 20:09:21 +0000

    [aGrUM] Adding job for doc generation

---
+++ wrappers/pyAgrum/wheelhouse/build_doc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get install -y pandoc
+
+root=$PWD
+
+virtualenv py38
+source py38/bin/activate
+
+python act clean
+
+echo "Install pyAgrum requirements..."
+pip install -r wrappers/pyAgrum/testunits/requirements.txt
+pip install -r wrappers/pyAgrum/doc/sphinx/requirements.txt
+pip uninstall -y pyAgrum
+
+echo "Building pyAgrum doc..."
+python act install release pyAgrum -j halfexcept1 -d $root/py38
+python act doc pyAgrum
+cd wrappers/pyAgrum/doc/sphinx
+make html
+
+echo "Script execution completed."
