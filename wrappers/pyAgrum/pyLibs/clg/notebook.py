cb2be76d175ef099cf1e99df4179a80143cb317e
Pierre-Henri Wuilemin <pierre-henri.wuillemin@lip6.fr>
2024-09-03 15:29:16 +0200

    [pyAgrum] standardized licenses in python files.

--- wrappers/pyAgrum/pyLibs/clg/notebook.py
+++ wrappers/pyAgrum/pyLibs/clg/notebook.py
@@ -1,5 +1,4 @@
 # (c) Copyright 2022-2023 by Pierre-Henri Wuillemin(@LIP6)  (pierre-henri.wuillemin@lip6.fr)
-
 # Permission to use, copy, modify, and distribute this
 # software and its documentation for any purpose and
 # without fee or royalty is hereby granted, provided
@@ -7,7 +6,6 @@
 # and that both that copyright notice and this permission
 # notice appear in supporting documentation or portions
 # thereof, including modifications, that you make.
-
 # THE AUTHOR P.H. WUILLEMIN  DISCLAIMS ALL WARRANTIES
 # WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT
--- wrappers/pyAgrum/pyLibs/lib/__init__.py
+++ wrappers/pyAgrum/pyLibs/lib/__init__.py
@@ -1,5 +1,4 @@
 # (c) 2016-2024 Copyright by Pierre-Henri Wuillemin,LIP6 (pierre-henri.wuillemin@lip6.fr)
-
 # Permission to use, copy, modify, and distribute this
 # software and its documentation for any purpose and
 # without fee or royalty is hereby granted, provided
@@ -7,7 +6,6 @@
 # and that both that copyright notice and this permission
 # notice appear in supporting documentation or portions
 # thereof, including modifications, that you make.
-
 # THE AUTHOR P.H. WUILLEMIN  DISCLAIMS ALL WARRANTIES
 # WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT
