e4ae4a45d75a01d062a24488a011496e6b962e36
Pierre-Henri Wuilemin <pierre-henri.wuillemin@lip6.fr>
2025-07-29 17:59:08 +0200

    [aGrUM] Add BNGumReader class for reading Bayesian networks from GUM files

---
+++ src/agrum/BN/io/GUM/BNGumReader.h
@@ -0,0 +1,91 @@
+/****************************************************************************
+ *   This file is part of the aGrUM/pyAgrum library.                        *
+ *                                                                          *
+ *   Copyright (c) 2005-2025 by                                             *
+ *       - Pierre-Henri WUILLEMIN(_at_LIP6)                                 *
+ *       - Christophe GONZALES(_at_AMU)                                     *
+ *                                                                          *
+ *   The aGrUM/pyAgrum library is free software; you can redistribute it    *
+ *   and/or modify it under the terms of either :                           *
+ *                                                                          *
+ *    - the GNU Lesser General Public License as published by               *
+ *      the Free Software Foundation, either version 3 of the License,      *
+ *      or (at your option) any later version,                              *
+ *    - the MIT license (MIT),                                              *
+ *    - or both in dual license, as here.                                   *
+ *                                                                          *
+ *   (see https://agrum.gitlab.io/articles/dual-licenses-lgplv3mit.html)    *
+ *                                                                          *
+ *   This aGrUM/pyAgrum library is distributed in the hope that it will be  *
+ *   useful, but WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,          *
+ *   INCLUDING BUT NOT LIMITED TO THE WARRANTIES MERCHANTABILITY or FITNESS *
+ *   FOR A PARTICULAR PURPOSE  AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,        *
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  *
+ *   OTHER DEALINGS IN THE SOFTWARE.                                        *
+ *                                                                          *
+ *   See LICENCES for more details.                                         *
+ *                                                                          *
+ *   SPDX-FileCopyrightText: Copyright 2005-2025                            *
+ *       - Pierre-Henri WUILLEMIN(_at_LIP6)                                 *
+ *       - Christophe GONZALES(_at_AMU)                                     *
+ *   SPDX-License-Identifier: LGPL-3.0-or-later OR MIT                      *
+ *                                                                          *
+ *   Contact  : info_at_agrum_dot_org                                       *
+ *   homepage : http://agrum.gitlab.io                                      *
+ *   gitlab   : https://gitlab.com/agrumery/agrum                           *
+ *                                                                          *
+ ****************************************************************************/
+
+
+#ifndef GUMREADER_H
+#define GUMREADER_H
+#include <agrum/BN/io/BNReader.h>
+
+namespace gum {
+  /**
+   * @class BNGumReader
+   * @headerfile BNGumReader.h <agrum/BN/io/GUM/BNGumReader.h>
+   * @ingroup bn_io
+   * @brief Class for reading a Bayesian network from a GUM (json) file.
+   *
+   * This class is used to read a Bayesian network from a GUM file format.
+   */
+  template <typename GUM_SCALAR>
+  class BNGumReader: public BNReader< GUM_SCALAR > {
+    public:
+    /**
+     * Constructor
+     * A reader is defined for reading a defined file. Hence the 2 args of the
+     * constructor.
+     * Note that the BN has to be built outside the reader. There is no
+     * delegation to create/destroy the BN from inside the reader.
+     */
+    BNGumReader(BayesNet< GUM_SCALAR >* bn, const std::string& filename);
+
+    /**
+     * Default destructor.
+     */
+    ~BNGumReader() override;
+
+    /// parse.
+    /// @return the number of detected errors
+    Size proceed() final;
+
+    protected:
+    BayesNet< GUM_SCALAR >* _bn_;
+    std::string             _streamName_;
+    bool                    _parseDone_;
+    bool                    _ioerror_;
+  };
+
+
+#ifndef GUM_NO_EXTERN_TEMPLATE_CLASS
+  extern template class BNGumReader< double >;
+#endif
+} /* namespace gum */
+
+#include <agrum/BN/io/GUM/BNGumReader_tpl.h>
+
+#endif   // GUM_BN_READER_H
---
+++ src/agrum/BN/io/GUM/BNGumReader_tpl.h
@@ -0,0 +1,64 @@
+/****************************************************************************
+ *   This file is part of the aGrUM/pyAgrum library.                        *
+ *                                                                          *
+ *   Copyright (c) 2005-2025 by                                             *
+ *       - Pierre-Henri WUILLEMIN(_at_LIP6)                                 *
+ *       - Christophe GONZALES(_at_AMU)                                     *
+ *                                                                          *
+ *   The aGrUM/pyAgrum library is free software; you can redistribute it    *
+ *   and/or modify it under the terms of either :                           *
+ *                                                                          *
+ *    - the GNU Lesser General Public License as published by               *
+ *      the Free Software Foundation, either version 3 of the License,      *
+ *      or (at your option) any later version,                              *
+ *    - the MIT license (MIT),                                              *
+ *    - or both in dual license, as here.                                   *
+ *                                                                          *
+ *   (see https://agrum.gitlab.io/articles/dual-licenses-lgplv3mit.html)    *
+ *                                                                          *
+ *   This aGrUM/pyAgrum library is distributed in the hope that it will be  *
+ *   useful, but WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,          *
+ *   INCLUDING BUT NOT LIMITED TO THE WARRANTIES MERCHANTABILITY or FITNESS *
+ *   FOR A PARTICULAR PURPOSE  AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,        *
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  *
+ *   OTHER DEALINGS IN THE SOFTWARE.                                        *
+ *                                                                          *
+ *   See LICENCES for more details.                                         *
+ *                                                                          *
+ *   SPDX-FileCopyrightText: Copyright 2005-2025                            *
+ *       - Pierre-Henri WUILLEMIN(_at_LIP6)                                 *
+ *       - Christophe GONZALES(_at_AMU)                                     *
+ *   SPDX-License-Identifier: LGPL-3.0-or-later OR MIT                      *
+ *                                                                          *
+ *   Contact  : info_at_agrum_dot_org                                       *
+ *   homepage : http://agrum.gitlab.io                                      *
+ *   gitlab   : https://gitlab.com/agrumery/agrum                           *
+ *                                                                          *
+ ****************************************************************************/
+
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+
+// to ease parsing in IDE
+#  include <agrum/BN/io/net/netWriter.h>
+
+namespace gum {
+  template <typename GUM_SCALAR>
+  BNGumReader<
+    GUM_SCALAR >::BNGumReader(BayesNet< GUM_SCALAR >* bn,
+                              const std::string& filename) : BNReader< GUM_SCALAR >(bn, filename) {
+    GUM_CONSTRUCTOR(BNGumReader)
+    _bn_         = bn;
+    _streamName_ = filename;
+    _parseDone_  = false;
+    _ioerror_    = false;
+  }
+
+  template <typename GUM_SCALAR>
+  BNGumReader< GUM_SCALAR >::~BNGumReader() { GUM_DESTRUCTOR(BNGumReader) }
+
+  template <typename GUM_SCALAR>
+  void BNGumReader< GUM_SCALAR >::read() {}
+}
