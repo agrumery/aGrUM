23f761372194bcb072dc46b0c6a02ad28d272ff2
Julien Schueller <schueller@phimeca.com>
2025-11-03 12:46:19 +0100

    o4DGContext: Disambiguate log2 namespace

--- src/agrum/base/multidim/utils/FunctionGraphUtilities/operators/o4DGContext.cpp
+++ src/agrum/base/multidim/utils/FunctionGraphUtilities/operators/o4DGContext.cpp
@@ -61,24 +61,24 @@ namespace gum {
   const Idx O4DGContext::_nbLogPrime_ = 140;
 
   const double O4DGContext::_logPrime_[]
-      = {log2(2),   log2(3),   log2(5),   log2(7),   log2(11),  log2(13),  log2(17),  log2(19),
-         log2(23),  log2(29),  log2(31),  log2(37),  log2(41),  log2(43),  log2(47),  log2(53),
-         log2(59),  log2(61),  log2(67),  log2(71),  log2(73),  log2(79),  log2(83),  log2(89),
-         log2(97),  log2(101), log2(103), log2(107), log2(109), log2(113), log2(127), log2(131),
-         log2(137), log2(139), log2(149), log2(151), log2(157), log2(163), log2(167), log2(173),
-         log2(179), log2(181), log2(191), log2(193), log2(197), log2(199), log2(211), log2(223),
-         log2(227), log2(229), log2(233), log2(239), log2(241), log2(251), log2(257), log2(263),
-         log2(269), log2(271), log2(277), log2(281), log2(283), log2(293), log2(307), log2(311),
-         log2(313), log2(317), log2(331), log2(337), log2(347), log2(349), log2(353), log2(359),
-         log2(367), log2(373), log2(379), log2(383), log2(389), log2(397), log2(401), log2(409),
-         log2(419), log2(421), log2(431), log2(433), log2(439), log2(443), log2(449), log2(457),
-         log2(461), log2(463), log2(467), log2(479), log2(487), log2(491), log2(499), log2(503),
-         log2(509), log2(521), log2(523), log2(541), log2(547), log2(557), log2(563), log2(569),
-         log2(571), log2(577), log2(587), log2(593), log2(599), log2(601), log2(607), log2(613),
-         log2(617), log2(619), log2(631), log2(641), log2(643), log2(647), log2(653), log2(659),
-         log2(661), log2(673), log2(677), log2(683), log2(691), log2(701), log2(709), log2(719),
-         log2(727), log2(733), log2(739), log2(743), log2(751), log2(757), log2(761), log2(769),
-         log2(773), log2(787), log2(797), log2(809)};
+      = {std::log2(2.0),   std::log2(3.0),   std::log2(5.0),   std::log2(7.0),   std::log2(11.0),  std::log2(13.0),  std::log2(17.0),  std::log2(19.0),
+         std::log2(23.0),  std::log2(29.0),  std::log2(31.0),  std::log2(37.0),  std::log2(41.0),  std::log2(43.0),  std::log2(47.0),  std::log2(53.0),
+         std::log2(59.0),  std::log2(61.0),  std::log2(67.0),  std::log2(71.0),  std::log2(73.0),  std::log2(79.0),  std::log2(83.0),  std::log2(89.0),
+         std::log2(97.0),  std::log2(101.0), std::log2(103.0), std::log2(107.0), std::log2(109.0), std::log2(113.0), std::log2(127.0), std::log2(131.0),
+         std::log2(137.0), std::log2(139.0), std::log2(149.0), std::log2(151.0), std::log2(157.0), std::log2(163.0), std::log2(167.0), std::log2(173.0),
+         std::log2(179.0), std::log2(181.0), std::log2(191.0), std::log2(193.0), std::log2(197.0), std::log2(199.0), std::log2(211.0), std::log2(223.0),
+         std::log2(227.0), std::log2(229.0), std::log2(233.0), std::log2(239.0), std::log2(241.0), std::log2(251.0), std::log2(257.0), std::log2(263.0),
+         std::log2(269.0), std::log2(271.0), std::log2(277.0), std::log2(281.0), std::log2(283.0), std::log2(293.0), std::log2(307.0), std::log2(311.0),
+         std::log2(313.0), std::log2(317.0), std::log2(331.0), std::log2(337.0), std::log2(347.0), std::log2(349.0), std::log2(353.0), std::log2(359.0),
+         std::log2(367.0), std::log2(373.0), std::log2(379.0), std::log2(383.0), std::log2(389.0), std::log2(397.0), std::log2(401.0), std::log2(409.0),
+         std::log2(419.0), std::log2(421.0), std::log2(431.0), std::log2(433.0), std::log2(439.0), std::log2(443.0), std::log2(449.0), std::log2(457.0),
+         std::log2(461.0), std::log2(463.0), std::log2(467.0), std::log2(479.0), std::log2(487.0), std::log2(491.0), std::log2(499.0), std::log2(503.0),
+         std::log2(509.0), std::log2(521.0), std::log2(523.0), std::log2(541.0), std::log2(547.0), std::log2(557.0), std::log2(563.0), std::log2(569.0),
+         std::log2(571.0), std::log2(577.0), std::log2(587.0), std::log2(593.0), std::log2(599.0), std::log2(601.0), std::log2(607.0), std::log2(613.0),
+         std::log2(617.0), std::log2(619.0), std::log2(631.0), std::log2(641.0), std::log2(643.0), std::log2(647.0), std::log2(653.0), std::log2(659.0),
+         std::log2(661.0), std::log2(673.0), std::log2(677.0), std::log2(683.0), std::log2(691.0), std::log2(701.0), std::log2(709.0), std::log2(719.0),
+         std::log2(727.0), std::log2(733.0), std::log2(739.0), std::log2(743.0), std::log2(751.0), std::log2(757.0), std::log2(761.0), std::log2(769.0),
+         std::log2(773.0), std::log2(787.0), std::log2(797.0), std::log2(809.0)};
 
   const Idx O4DGContext::_offsetv_ = O4DGContext::_nbLogPrime_ - 3;
   const Idx O4DGContext::_offset1_ = O4DGContext::_nbLogPrime_ - 1;
